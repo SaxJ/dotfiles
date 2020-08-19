31a37d417f72255a0c92274ac768239d076f72fa
Saxon Jensen <saxon.jensen@healthengine.com.au>
2020-08-19 22:14:18 +0800

    Update emacs

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -28,7 +28,6 @@
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
 (package! string-inflection)
-(package! wakatime-mode)
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
