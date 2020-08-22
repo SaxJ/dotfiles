ae02a5278d8f271f3ac1d9d9779e0eca2e99938d
Saxon Jensen <saxon.jensen@healthengine.com.au>
2020-08-22 17:58:51 +0800

    Graphql emacs

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -28,6 +28,7 @@
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
 (package! string-inflection)
+(package! graphql-mode)
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
