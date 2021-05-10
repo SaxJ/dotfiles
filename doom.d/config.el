6c6fcd25ea6d16f89025b4f498ce9c210027278e
Saxon Jensen <saxon.jensen@healthengine.com.au>
2021-05-10 10:25:13 +0800

    Remove team calendar

--- doom.d/config.el
+++ doom.d/config.el
@@ -79,7 +79,6 @@
    :contents-sources
    (list
     (cfw:org-create-source "Green")  ; org-agenda source
-    (cfw:ical-create-source "Team" (replace-regexp-in-string "\n\\'" "" (shell-command-to-string "pass calendar/team")) "IndianRed")
     (cfw:ical-create-source "Work" (replace-regexp-in-string "\n\\'" "" (shell-command-to-string "pass calendar/work")) "Blue")
     (cfw:ical-create-source "Comps" "https://calendar.google.com/calendar/ical/iu1iul1u3n8ic3s78f4df15u4o%40group.calendar.google.com/public/basic.ics" "Orange")
     )))
