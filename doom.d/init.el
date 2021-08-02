35ea5ab18297af45d14134444f1ff09c11a6a1bb
Saxon Jensen <saxon.jensen@healthengine.com.au>
2021-08-03 00:07:20 +0800

    Ignore doom snippets

--- doom.d/init.el
+++ doom.d/init.el
@@ -33,11 +33,11 @@
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       hydra
+       ;;hydra
        (emoji +github +unicode)
        ;;indent-guides     ; highlighted indent columns
        minimap           ; show a map of the code on the side
-       (modeline +light)   ; snazzy, Atom-inspired modeline, plus API
+       modeline   ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
