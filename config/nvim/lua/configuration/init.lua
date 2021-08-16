9aaf01e5de70d734d02fa2f3a005c43c431eb559
Saxon Jensen <saxon.jensen@healthengine.com.au>
2021-08-16 20:20:02 +0800

    Telescope bindings

--- config/nvim/lua/configuration/init.lua
+++ config/nvim/lua/configuration/init.lua
@@ -1,20 +1,27 @@
-local util = require('utils')
+local util = require("utils")
 
-require('configuration/completion')
-require('configuration/lsp')
-require('configuration/formatting')
-require('lualine').setup({theme = 'onedark'})
+require("configuration/completion")
+require("configuration/lsp")
+require("configuration/formatting")
+require("lualine").setup({theme = "onedark"})
 
-require('telescope').setup{
+local actions = require "telescope.actions"
+require("telescope").setup {
   defaults = {
+    mappings = {
+      i = {
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous
+      }
+    },
     vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case'
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case"
     },
     prompt_prefix = "> ",
     selection_caret = "> ",
@@ -25,33 +32,32 @@ require('telescope').setup{
     layout_strategy = "horizontal",
     layout_config = {
       horizontal = {
-        mirror = false,
+        mirror = false
       },
       vertical = {
-        mirror = false,
-      },
+        mirror = false
+      }
     },
-    file_sorter =  require'telescope.sorters'.get_fuzzy_file,
+    file_sorter = require "telescope.sorters".get_fuzzy_file,
     file_ignore_patterns = {},
-    generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+    generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
     winblend = 0,
     border = {},
-    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+    borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
     color_devicons = true,
     use_less = true,
     path_display = {},
-    set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
-    file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-    qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
-
+    set_env = {["COLORTERM"] = "truecolor"}, -- default = nil,
+    file_previewer = require "telescope.previewers".vim_buffer_cat.new,
+    grep_previewer = require "telescope.previewers".vim_buffer_vimgrep.new,
+    qflist_previewer = require "telescope.previewers".vim_buffer_qflist.new,
     -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+    buffer_previewer_maker = require "telescope.previewers".buffer_previewer_maker
   }
 }
 
-local saga = require 'lspsaga'
+local saga = require "lspsaga"
 saga.init_lsp_saga()
 
-local neogit = require('neogit')
+local neogit = require("neogit")
 neogit.setup {}
