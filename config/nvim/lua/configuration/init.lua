59d79cef3bf8fd162657c1f47c99f9c69fb66ded
Saxon Jensen <saxon.jensen@healthengine.com.au>
2021-09-09 01:16:28 +0800

    Show diagnostics in status line

--- config/nvim/lua/configuration/init.lua
+++ config/nvim/lua/configuration/init.lua
@@ -8,7 +8,21 @@ require("lualine").setup(
     theme = "onedark",
     sections = {
       lualine_c = {
-        {"filename", path = 1}
+        {"filename", path = 1},
+        {
+          "diagnostics",
+          -- table of diagnostic sources, available sources:
+          -- nvim_lsp, coc, ale, vim_lsp
+          sources = {"nvim_lsp"},
+          -- displays diagnostics from defined severity
+          sections = {"error", "warn", "info", "hint"},
+          -- all colors are in format #rrggbb
+          --color_error = nil, -- changes diagnostic's error foreground color
+          --color_warn = nil, -- changes diagnostic's warn foreground color
+          --color_info = nil, -- Changes diagnostic's info foreground color
+          --color_hint = nil, -- Changes diagnostic's hint foreground color
+          symbols = {error = "E", warn = "W", info = "I", hint = "H"}
+        }
       }
     }
   }
