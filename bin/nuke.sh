29ed282333de0b82fb451fedd4d82fef48ebf850
Saxon Jensen <saxon.jensen@healthengine.com.au>
2021-09-24 12:10:12 +0800

    Update nuke

--- bin/nuke.sh
+++ bin/nuke.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
- 
+
 # Script to remove everything that Minikube adds to a system when it sets the hypervisor = 'none'
- 
+
 if [[ $UID != 0 ]]
 then
         echo Must run as root
         exit 1
 fi
- 
+
 # Point home at the user's home area if they are using sudo so the files there are processed.
 [[ -n $SUDO_USER ]] && HOME=$(eval echo "~$SUDO_USER")
- 
+
 minikube delete
 kubectl config delete-cluster minikube
 kubectl config delete-context minikube
 systemctl stop kubelet.service
 mount | awk '/kubelet/ {print $3}' | xargs -i -n 1 umount {}
- 
+
 rm -rf \
         "$HOME/.minikube" \
         /bin/kubeadm \
@@ -27,6 +27,7 @@ rm -rf \
         /lib/systemd/system/kubelet.service \
         /var/lib/minikube \
         /var/lib/kubelet \
-        /tmp/juju-*
- 
+        /tmp/juju-* \
+        /tmp/hostpath-provisioner/*
+
 systemctl daemon-reload
