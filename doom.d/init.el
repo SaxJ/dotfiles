e54c0e45b92e401de8c92cba7230db852ecaaf1d
Saxon Jensen <saxon.jensen@healthengine.com.au>
2021-10-06 15:59:43 +0800

    Don't need extra ligatures

--- doom.d/init.el
+++ doom.d/init.el
@@ -41,7 +41,7 @@
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       (ligatures +extra)       ; ligatures or substitute text with pretty symbols
+       ligatures      ; ligatures or substitute text with pretty symbols
        ;;tabs              ; a tab bar for Emacs
        (treemacs +lsp)          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
