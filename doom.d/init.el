6d0a386034f7801874cf4600b5f97b4a943be794
Saxon Jensen <saxon.jensen@healthengine.com.au>
2021-10-11 11:12:31 +0800

    Minimap is useless

--- doom.d/init.el
+++ doom.d/init.el
@@ -35,7 +35,7 @@
        ;;hydra
        (emoji +github +unicode)
        ;;indent-guides     ; highlighted indent columns
-       minimap           ; show a map of the code on the side
+       ;;minimap           ; show a map of the code on the side
        modeline   ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
