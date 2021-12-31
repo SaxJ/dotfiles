f90ad1e44ebab7240be334fda9fd9fc94dffb16b
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-01-01 01:45:56 +0800

    Add a better fsharp language server

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -41,8 +41,6 @@
 (package! slack)
 (package! system-packages)
 (package! org-jira)
-(package! dtache
-  :recipe (:host gitlab :repo "niklaseklund/dtache"))
 (package! dirvish)
 (package! telega
   :recipe (:host github :repo "zevlg/telega.el" :branch "releases" :files (:defaults "contrib" "etc" "server" "Makefile")))
