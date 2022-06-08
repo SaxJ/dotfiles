403011b7543055500532c2618c331b844259c04e
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-06-08 21:41:40 +0800

    Better lsp optimisation

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -5,8 +5,7 @@
 
 ;;; Code:
 ;; Performance
-(setq gc-cons-threshold 100000000
-      read-process-output-max 3000000)
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
 
 (setq doom-localleader-key ",")
 
