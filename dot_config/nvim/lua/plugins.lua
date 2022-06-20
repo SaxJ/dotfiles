182e22303297d7afa9c51d2d7a6d160bbac69f22
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-06-20 16:34:07 +0800

    Easier git flow

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -230,7 +230,9 @@ return require("packer").startup(function(use)
             require('neogit').setup({
                 integrations = {
                     diffview = true
-                }
+                },
+                disable_commit_confirmation = true,
+                disable_insert_on_commit = false
             })
         end
     })
