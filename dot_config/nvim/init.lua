a232e239601dab06d226d252f72ef906cb06c183
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-06-27 11:25:01 +0800

    Formatting and switch to neotree

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -7,144 +7,159 @@ require("settings")
 -- Plugin Config
 require("configuration")
 
-local wk = require('which-key')
-local mapx = require "mapx"
+local wk = require("which-key")
+local mapx = require("mapx")
 
-local Terminal = require('toggleterm.terminal').Terminal
+local Terminal = require("toggleterm.terminal").Terminal
 local mail = Terminal:new({
-    dir = '~',
-    cmd = 'aerc',
-    hidden = true,
-    close_on_exit = true,
-    direction = 'float',
+	dir = "~",
+	cmd = "aerc",
+	hidden = true,
+	close_on_exit = true,
+	direction = "float",
 })
 local tasks = Terminal:new({
-    dir = '~',
-    cmd = 'taskwarrior-tui',
-    hidden = true,
-    close_on_exit = true,
-    direction = 'float',
+	dir = "~",
+	cmd = "taskwarrior-tui",
+	hidden = true,
+	close_on_exit = true,
+	direction = "float",
 })
 local floatingTerminal = Terminal:new({
-    direction = 'float'
+	direction = "float",
 })
 
 -- Misc
 wk.register({
-    b = {
-        name = 'buffers',
-        b = { "<cmd>Telescope buffers<cr>", "Buffers" }
-    },
-    o = {
-        name = "open",
-        m = { function() mail:toggle() end, "Mail" },
-        T = { function() floatingTerminal:toggle() end, "Terminal" },
-        p = { "NvimTreeToggle<cr>", "Project" },
-        ['-'] = { ':NnnPicker %:p:h<CR>', 'Files' }
-    },
-    ["<leader>"] = { "<cmd>Telescope find_files<cr>", "Recent Files" },
-    ["<tab>"] = { "<cmd>Telescope buffers<cr>", "Buffers" },
-    f = {
-        name = "files",
-        f = { "<cmd>Telescope find_files<cr>", "Files" },
-    },
-    s = {
-        name = "search",
-        p = { "<cmd>Telescope live_grep<cr>", "Search Project" },
-    },
-    p = {
-        name = "project",
-        p = { ":Telescope projects<cr>", "Projects" },
-        t = { ":TodoTelescope<cr>", "Todos" },
-        m = { ":Telescope harpoon marks<cr>", "Marks" }
-    },
-    g = {
-        name = "git",
-        b = { ":GitBlameToggle<cr>", "Blame" },
-        g = { ':Neogit<CR>', "Open Git" },
-        h = {
-            name = "+Github",
-            c = {
-                name = "+Commits",
-                c = { "<cmd>GHCloseCommit<cr>", "Close" },
-                e = { "<cmd>GHExpandCommit<cr>", "Expand" },
-                o = { "<cmd>GHOpenToCommit<cr>", "Open To" },
-                p = { "<cmd>GHPopOutCommit<cr>", "Pop Out" },
-                z = { "<cmd>GHCollapseCommit<cr>", "Collapse" },
-            },
-            i = {
-                name = "+Issues",
-                p = { "<cmd>GHPreviewIssue<cr>", "Preview" },
-            },
-            l = {
-                name = "+Litee",
-                t = { "<cmd>LTPanel<cr>", "Toggle Panel" },
-            },
-            r = {
-                name = "+Review",
-                b = { "<cmd>GHStartReview<cr>", "Begin" },
-                c = { "<cmd>GHCloseReview<cr>", "Close" },
-                d = { "<cmd>GHDeleteReview<cr>", "Delete" },
-                e = { "<cmd>GHExpandReview<cr>", "Expand" },
-                s = { "<cmd>GHSubmitReview<cr>", "Submit" },
-                z = { "<cmd>GHCollapseReview<cr>", "Collapse" },
-            },
-            p = {
-                name = "+Pull Request",
-                c = { "<cmd>GHClosePR<cr>", "Close" },
-                d = { "<cmd>GHPRDetails<cr>", "Details" },
-                e = { "<cmd>GHExpandPR<cr>", "Expand" },
-                o = { "<cmd>GHOpenPR<cr>", "Open" },
-                p = { "<cmd>GHPopOutPR<cr>", "PopOut" },
-                r = { "<cmd>GHRefreshPR<cr>", "Refresh" },
-                t = { "<cmd>GHOpenToPR<cr>", "Open To" },
-                z = { "<cmd>GHCollapsePR<cr>", "Collapse" },
-            },
-            t = {
-                name = "+Threads",
-                c = { "<cmd>GHCreateThread<cr>", "Create" },
-                n = { "<cmd>GHNextThread<cr>", "Next" },
-                t = { "<cmd>GHToggleThread<cr>", "Toggle" },
-            },
-        },
-    },
-    t = {
-        name = "toggle",
-        t = { ":ToggleTerm size=30<cr>", "Terminal" },
-    },
-    T = {
-        name = 'tasks',
-        t = { function() tasks:toggle() end, 'Taskwarrior' },
-    },
-    n = {
-        name = "notes",
-        v = { ':Neorg gtd views<cr>', 'View Notes' },
-        c = { ':Neorg gtd capture<cr>', 'Create Note' },
-        j = {
-            name = "journal",
-            t = { ':Neorg journal today<cr>', 'Today' },
-            y = { ':Neorg journal yesterday<cr>', 'Yesterday' },
-            n = { ':Neorg journal tomorrow<cr>', 'Tomorrow' },
-            c = { ':Neorg journal ', 'Create' },
-        }
-    },
-    r = {
-        name = 'remote',
-        u = { ':call SyncUploadFile()<cr>', 'Upload' },
-        d = { ':call SyncDownloadFile()<cr>', 'Download' },
-    }
+	b = {
+		name = "buffers",
+		b = { "<cmd>Telescope buffers<cr>", "Buffers" },
+	},
+	o = {
+		name = "open",
+		m = {
+			function()
+				mail:toggle()
+			end,
+			"Mail",
+		},
+		T = {
+			function()
+				floatingTerminal:toggle()
+			end,
+			"Terminal",
+		},
+		p = { "NvimTreeToggle<cr>", "Project" },
+		["-"] = { ":Neotree<CR>", "Files" },
+	},
+	["<leader>"] = { "<cmd>Telescope find_files<cr>", "Recent Files" },
+	["<tab>"] = { "<cmd>Telescope buffers<cr>", "Buffers" },
+	f = {
+		name = "files",
+		f = { "<cmd>Telescope find_files<cr>", "Files" },
+	},
+	s = {
+		name = "search",
+		p = { "<cmd>Telescope live_grep<cr>", "Search Project" },
+	},
+	p = {
+		name = "project",
+		p = { ":Telescope projects<cr>", "Projects" },
+		t = { ":TodoTelescope<cr>", "Todos" },
+		m = { ":Telescope harpoon marks<cr>", "Marks" },
+	},
+	g = {
+		name = "git",
+		b = { ":GitBlameToggle<cr>", "Blame" },
+		g = { ":Neogit<CR>", "Open Git" },
+		h = {
+			name = "+Github",
+			c = {
+				name = "+Commits",
+				c = { "<cmd>GHCloseCommit<cr>", "Close" },
+				e = { "<cmd>GHExpandCommit<cr>", "Expand" },
+				o = { "<cmd>GHOpenToCommit<cr>", "Open To" },
+				p = { "<cmd>GHPopOutCommit<cr>", "Pop Out" },
+				z = { "<cmd>GHCollapseCommit<cr>", "Collapse" },
+			},
+			i = {
+				name = "+Issues",
+				p = { "<cmd>GHPreviewIssue<cr>", "Preview" },
+			},
+			l = {
+				name = "+Litee",
+				t = { "<cmd>LTPanel<cr>", "Toggle Panel" },
+			},
+			r = {
+				name = "+Review",
+				b = { "<cmd>GHStartReview<cr>", "Begin" },
+				c = { "<cmd>GHCloseReview<cr>", "Close" },
+				d = { "<cmd>GHDeleteReview<cr>", "Delete" },
+				e = { "<cmd>GHExpandReview<cr>", "Expand" },
+				s = { "<cmd>GHSubmitReview<cr>", "Submit" },
+				z = { "<cmd>GHCollapseReview<cr>", "Collapse" },
+			},
+			p = {
+				name = "+Pull Request",
+				c = { "<cmd>GHClosePR<cr>", "Close" },
+				d = { "<cmd>GHPRDetails<cr>", "Details" },
+				e = { "<cmd>GHExpandPR<cr>", "Expand" },
+				o = { "<cmd>GHOpenPR<cr>", "Open" },
+				p = { "<cmd>GHPopOutPR<cr>", "PopOut" },
+				r = { "<cmd>GHRefreshPR<cr>", "Refresh" },
+				t = { "<cmd>GHOpenToPR<cr>", "Open To" },
+				z = { "<cmd>GHCollapsePR<cr>", "Collapse" },
+			},
+			t = {
+				name = "+Threads",
+				c = { "<cmd>GHCreateThread<cr>", "Create" },
+				n = { "<cmd>GHNextThread<cr>", "Next" },
+				t = { "<cmd>GHToggleThread<cr>", "Toggle" },
+			},
+		},
+	},
+	t = {
+		name = "toggle",
+		t = { ":ToggleTerm size=30<cr>", "Terminal" },
+	},
+	T = {
+		name = "tasks",
+		t = {
+			function()
+				tasks:toggle()
+			end,
+			"Taskwarrior",
+		},
+	},
+	n = {
+		name = "notes",
+		v = { ":Neorg gtd views<cr>", "View Notes" },
+		c = { ":Neorg gtd capture<cr>", "Create Note" },
+		j = {
+			name = "journal",
+			t = { ":Neorg journal today<cr>", "Today" },
+			y = { ":Neorg journal yesterday<cr>", "Yesterday" },
+			n = { ":Neorg journal tomorrow<cr>", "Tomorrow" },
+			c = { ":Neorg journal ", "Create" },
+		},
+	},
+	r = {
+		name = "remote",
+		u = { ":call SyncUploadFile()<cr>", "Upload" },
+		d = { ":call SyncDownloadFile()<cr>", "Download" },
+	},
 }, { prefix = "<leader>" })
 
 -- misc
-mapx.nnoremap("<C-e>", ":NnnExplorer %:p:h<CR>")
+mapx.nnoremap("<C-e>", ":Neotree<CR>")
 
 -- Terminal
-mapx.tnoremap('<Esc>', '<C-\\><C-n>')
+mapx.tnoremap("<Esc>", "<C-\\><C-n>")
 
 -- FileType Specific --
 -- HTTP
 mapx.nmap("<localleader>e", "<Plug>RestNvim", "silent", { ft = "http" })
-mapx.nmap('<localleader>d', ":put =strftime('%m/%d/%y')<cr>", "silent", { ft = "norg" })
+mapx.nmap("<localleader>d", ":put =strftime('%m/%d/%y')<cr>", "silent", { ft = "norg" })
 
 -- autorun
-vim.cmd [[silent! NeorgStart silent=true]]
+vim.cmd([[silent! NeorgStart silent=true]])
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -4,307 +4,325 @@ local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
-    execute("packadd packer.nvim")
+	fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
+	execute("packadd packer.nvim")
 end
 
 vim.cmd("packadd packer.nvim")
 
 -- Package installation
 return require("packer").startup(function(use)
-    -- Packer can manage itself
-    use("wbthomason/packer.nvim")
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-    -- Libraries
-    use("b0o/mapx.nvim")
-    use({
-        'echasnovski/mini.nvim',
-        config = function()
-            require('mini.surround').setup({})
-            require('mini.comment').setup({})
-            require('mini.cursorword').setup({})
-            require('mini.indentscope').setup({})
-            require('mini.jump').setup({})
-            require('mini.pairs').setup({})
-        end
-    })
-    use "folke/lua-dev.nvim"
+	-- Libraries
+	use("b0o/mapx.nvim")
+	use({
+		"echasnovski/mini.nvim",
+		config = function()
+			require("mini.surround").setup({})
+			require("mini.comment").setup({})
+			require("mini.cursorword").setup({})
+			require("mini.indentscope").setup({})
+			require("mini.jump").setup({})
+			require("mini.pairs").setup({})
+		end,
+	})
+	use("folke/lua-dev.nvim")
 
-    -- Appearance
-    use("folke/tokyonight.nvim")
-    use({
-        "hoob3rt/lualine.nvim",
-        requires = { "kyazdani42/nvim-web-devicons", opt = true },
-    })
+	-- Appearance
+	use("folke/tokyonight.nvim")
+	use({
+		"hoob3rt/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
 
-    -- Extra Languages
-    use("amadeus/vim-mjml")
-    use('rescript-lang/vim-rescript')
-    use({
-        "folke/todo-comments.nvim",
-        requires = "nvim-lua/plenary.nvim",
-        config = function()
-            require("todo-comments").setup({})
-        end,
-    })
-    use("adamclerk/vim-razor")
-    use("jparise/vim-graphql")
-    use("norcalli/nvim-colorizer.lua")
+	-- Extra Languages
+	use("amadeus/vim-mjml")
+	use("rescript-lang/vim-rescript")
+	use({
+		"folke/todo-comments.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("todo-comments").setup({})
+		end,
+	})
+	use("adamclerk/vim-razor")
+	use("jparise/vim-graphql")
+	use("norcalli/nvim-colorizer.lua")
 
-    -- LSP
-    use({
-        "tami5/lspsaga.nvim",
-        config = function()
-            local saga = require('lspsaga')
-            saga.setup({})
-        end
-    })
-    use({ 'junnplus/nvim-lsp-setup',
-        requires = {
-            'neovim/nvim-lspconfig',
-            'williamboman/nvim-lsp-installer',
-            'hrsh7th/vim-vsnip',
-        },
-        config = function()
-            require('nvim-lsp-setup').setup({
-                default_mappings = true,
-                gr = "lua require('goto-preview').goto_preview_references()",
-                gi = "lua require('goto-preview').goto_preview_implementation()",
-                on_attach = function(client, bufnr)
-                    --require('nvim-lsp-setup.utils').format_on_save(client)
-                end,
-                capabilities = vim.lsp.protocol.make_client_capabilities(),
-                servers = {
-                    sumneko_lua = require('lua-dev').setup(),
-                    tsserver = {
-                        init_options = {
-                            preferences = {
-                                importModuleSpecifierPreference = "relative"
-                            }
-                        }
-                    },
-                    intelephense = {
-                        init_options = {
-                            licenceKey = '/home/saxonj/intelephense/licence.txt'
-                        }
-                    },
-                    jsonls = {
-                        schemas = require('schemastore').json.schemas(),
-                        validate = { enable = true },
-                    },
-                    omnisharp = {
-                        cmd = {
-                            '/usr/bin/omnisharp',
-                            '-lsp',
-                            '-v',
-                            '--hostPID',
-                            tostring(vim.fn.getpid()),
-                        }
-                    }
-                }
-            })
-        end
-    })
-    use {
-        'rmagatti/goto-preview',
-        config = function()
-            require('goto-preview').setup({})
-        end
-    }
-    use('b0o/schemastore.nvim')
-    use {
-        "someone-stole-my-name/yaml-companion.nvim",
-        requires = {
-            { "neovim/nvim-lspconfig" },
-            { "nvim-lua/plenary.nvim" },
-            { "nvim-telescope/telescope.nvim" },
-        },
-        config = function()
-            require("telescope").load_extension("yaml_schema")
-        end,
-    }
+	-- LSP
+	use({
+		"tami5/lspsaga.nvim",
+		config = function()
+			local saga = require("lspsaga")
+			saga.setup({})
+		end,
+	})
+	use({
+		"junnplus/nvim-lsp-setup",
+		requires = {
+			"neovim/nvim-lspconfig",
+			"williamboman/nvim-lsp-installer",
+			"hrsh7th/vim-vsnip",
+		},
+		config = function()
+			require("nvim-lsp-setup").setup({
+				default_mappings = true,
+				gr = "lua require('goto-preview').goto_preview_references()",
+				gi = "lua require('goto-preview').goto_preview_implementation()",
+				on_attach = function(client, bufnr)
+					--require('nvim-lsp-setup.utils').format_on_save(client)
+				end,
+				capabilities = vim.lsp.protocol.make_client_capabilities(),
+				servers = {
+					sumneko_lua = require("lua-dev").setup(),
+					tsserver = {
+						init_options = {
+							preferences = {
+								importModuleSpecifierPreference = "relative",
+							},
+						},
+					},
+					intelephense = {
+						init_options = {
+							licenceKey = "/home/saxonj/intelephense/licence.txt",
+						},
+					},
+					jsonls = {
+						schemas = require("schemastore").json.schemas(),
+						validate = { enable = true },
+					},
+					omnisharp = {
+						cmd = {
+							"/usr/bin/omnisharp",
+							"-lsp",
+							"-v",
+							"--hostPID",
+							tostring(vim.fn.getpid()),
+						},
+					},
+					elmls = { { cmd = "elm-language-server" } },
+				},
+			})
+		end,
+	})
+	use({
+		"rmagatti/goto-preview",
+		config = function()
+			require("goto-preview").setup({})
+		end,
+	})
+	use("b0o/schemastore.nvim")
+	use({
+		"someone-stole-my-name/yaml-companion.nvim",
+		requires = {
+			{ "neovim/nvim-lspconfig" },
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope.nvim" },
+		},
+		config = function()
+			require("telescope").load_extension("yaml_schema")
+		end,
+	})
 
-    -- General code plugins
-    use("gpanders/editorconfig.nvim")
-    use("mhartington/formatter.nvim")
-    use({
-        "hrsh7th/nvim-cmp",
-        requires = {
-            "hrsh7th/vim-vsnip",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-nvim-lua",
-            "hrsh7th/cmp-nvim-lsp",
-        },
-    })
-    use({ "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/plenary.nvim" } } })
-    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-    use('airblade/vim-rooter')
-    use({
-        "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = "all",
-                highlight = {
-                    enable = true,
-                },
-                indent = {
-                    enable = true,
-                },
-            })
-        end,
-    })
-    use("nvim-treesitter/nvim-treesitter-context")
+	-- General code plugins
+	use("gpanders/editorconfig.nvim")
+	use("mhartington/formatter.nvim")
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			"hrsh7th/vim-vsnip",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+	})
+	use({ "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/plenary.nvim" } } })
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use("airblade/vim-rooter")
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = "all",
+				highlight = {
+					enable = true,
+				},
+				indent = {
+					enable = true,
+				},
+			})
+		end,
+	})
+	use("nvim-treesitter/nvim-treesitter-context")
 
-    -- Debuggers
-    use("mfussenegger/nvim-dap")
-    use("theHamsta/nvim-dap-virtual-text")
-    use({ "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } })
+	-- Debuggers
+	use("mfussenegger/nvim-dap")
+	use("theHamsta/nvim-dap-virtual-text")
+	use({ "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } })
 
-    -- Terminal
-    use { "akinsho/toggleterm.nvim", tag = 'v2.*', config = function()
-        require("toggleterm").setup()
-    end }
+	-- Terminal
+	use({
+		"akinsho/toggleterm.nvim",
+		tag = "v2.*",
+		config = function()
+			require("toggleterm").setup()
+		end,
+	})
 
-    -- Tooling
-    use {
-        "luukvbaal/nnn.nvim",
-        config = function() require("nnn").setup() end
-    }
-    use({
-        "NTBBloodbath/rest.nvim",
-        requires = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("rest-nvim").setup({
-                -- Open request results in a horizontal split
-                result_split_horizontal = false,
-                -- Skip SSL verification, useful for unknown certificates
-                skip_ssl_verification = true,
-                -- Highlight request on run
-                highlight = {
-                    enabled = true,
-                    timeout = 150,
-                },
-                -- Jump to request line on run
-                jump_to_request = false,
-            })
-            vim.cmd("autocmd FileType http nmap <buffer> <Enter> <Plug>RestNvim")
-        end,
-    })
-    use({
-        "nvim-neorg/neorg",
-        requires = "nvim-lua/plenary.nvim",
-    })
-    use("eshion/vim-sync")
-    use({
-        "folke/which-key.nvim",
-        config = function()
-            require("which-key").setup()
-        end,
-    })
-    use {
-        'glacambre/firenvim',
-        run = function() vim.fn['firenvim#install'](0) end
-    }
-    use('jghauser/mkdir.nvim')
+	-- Tooling
+	use({
+		"nvim-neo-tree/neo-tree.nvim",
 
-    -- Code navigation
-    use {
-        'ThePrimeagen/harpoon',
-        requires = { 'nvim-lua/plenary.nvim' }
-    }
-    use({
-        'ahmedkhalf/project.nvim',
-        config = function()
-            require('project_nvim').setup({})
-        end
-    })
+		branch = "v2.x",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+			require("neo-tree").setup({})
+		end,
+	})
+	use({
+		"NTBBloodbath/rest.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("rest-nvim").setup({
+				-- Open request results in a horizontal split
+				result_split_horizontal = false,
+				-- Skip SSL verification, useful for unknown certificates
+				skip_ssl_verification = true,
+				-- Highlight request on run
+				highlight = {
+					enabled = true,
+					timeout = 150,
+				},
+				-- Jump to request line on run
+				jump_to_request = false,
+			})
+			vim.cmd("autocmd FileType http nmap <buffer> <Enter> <Plug>RestNvim")
+		end,
+	})
+	use({
+		"nvim-neorg/neorg",
+		requires = "nvim-lua/plenary.nvim",
+	})
+	use("eshion/vim-sync")
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			require("which-key").setup()
+		end,
+	})
+	use({
+		"glacambre/firenvim",
+		run = function()
+			vim.fn["firenvim#install"](0)
+		end,
+	})
+	use("jghauser/mkdir.nvim")
 
-    -- Version Control
-    use({
-        'TimUntersberger/neogit',
-        requires = {
-            'nvim-lua/plenary.nvim',
-            'sindrets/diffview.nvim',
-        },
-        config = function()
-            require('neogit').setup({
-                integrations = {
-                    diffview = true
-                },
-                disable_commit_confirmation = true,
-                disable_insert_on_commit = false
-            })
-        end
-    })
-    use({
-        'ldelossa/gh.nvim',
-        requires = { { 'ldelossa/litee.nvim' } },
-        config = function()
-            require('litee.lib').setup()
-            require('litee.gh').setup({
-                -- remap the arrow keys to resize any litee.nvim windows.
-                map_resize_keys = false,
-                -- do not map any keys inside any gh.nvim buffers.
-                disable_keymaps = false,
-                -- the icon set to use.
-                icon_set = "default",
-                -- any custom icons to use.
-                icon_set_custom = nil,
-                -- whether to register the @username and #issue_number omnifunc completion
-                -- in buffers which start with .git/
-                git_buffer_completion = true,
-                -- defines keymaps in gh.nvim buffers.
-                keymaps = {
-                    -- when inside a gh.nvim panel, this key will open a node if it has
-                    -- any futher functionality. for example, hitting <CR> on a commit node
-                    -- will open the commit's changed files in a new gh.nvim panel.
-                    open = "<CR>",
-                    -- when inside a gh.nvim panel, expand a collapsed node
-                    expand = "zo",
-                    -- when inside a gh.nvim panel, collpased and expanded node
-                    collapse = "zc",
-                    -- when cursor is over a "#1234" formatted issue or PR, open its details
-                    -- and comments in a new tab.
-                    goto_issue = "gd",
-                    -- show any details about a node, typically, this reveals commit messages
-                    -- and submitted review bodys.
-                    details = "d",
-                    -- inside a convo buffer, submit a comment
-                    submit_comment = "<C-s>",
-                    -- inside a convo buffer, when your cursor is ontop of a comment, open
-                    -- up a set of actions that can be performed.
-                    actions = "<C-a>",
-                    -- inside a thread convo buffer, resolve the thread.
-                    resolve_thread = "<C-r>",
-                    -- inside a gh.nvim panel, if possible, open the node's web URL in your
-                    -- browser. useful particularily for digging into external failed CI
-                    -- checks.
-                    goto_web = "gx"
-                }
-            })
-        end
-    })
-    use({
-        "f-person/git-blame.nvim",
-        config = function()
-            vim.g.gitblame_enabled = 0
-        end,
-    })
-    use({
-        "lewis6991/gitsigns.nvim",
-        requires = {
-            "nvim-lua/plenary.nvim",
-        },
-        config = function()
-            require("gitsigns").setup()
-        end,
-    })
-    use {
-        'ruifm/gitlinker.nvim',
-        requires = 'nvim-lua/plenary.nvim',
-        config = function()
-            require "gitlinker".setup()
-        end
-    }
+	-- Code navigation
+	use({
+		"ThePrimeagen/harpoon",
+		requires = { "nvim-lua/plenary.nvim" },
+	})
+	use({
+		"ahmedkhalf/project.nvim",
+		config = function()
+			require("project_nvim").setup({})
+		end,
+	})
+
+	-- Version Control
+	use({
+		"TimUntersberger/neogit",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+		},
+		config = function()
+			require("neogit").setup({
+				integrations = {
+					diffview = true,
+				},
+				disable_commit_confirmation = true,
+				disable_insert_on_commit = false,
+			})
+		end,
+	})
+	use({
+		"ldelossa/gh.nvim",
+		requires = { { "ldelossa/litee.nvim" } },
+		config = function()
+			require("litee.lib").setup()
+			require("litee.gh").setup({
+				-- remap the arrow keys to resize any litee.nvim windows.
+				map_resize_keys = false,
+				-- do not map any keys inside any gh.nvim buffers.
+				disable_keymaps = false,
+				-- the icon set to use.
+				icon_set = "default",
+				-- any custom icons to use.
+				icon_set_custom = nil,
+				-- whether to register the @username and #issue_number omnifunc completion
+				-- in buffers which start with .git/
+				git_buffer_completion = true,
+				-- defines keymaps in gh.nvim buffers.
+				keymaps = {
+					-- when inside a gh.nvim panel, this key will open a node if it has
+					-- any futher functionality. for example, hitting <CR> on a commit node
+					-- will open the commit's changed files in a new gh.nvim panel.
+					open = "<CR>",
+					-- when inside a gh.nvim panel, expand a collapsed node
+					expand = "zo",
+					-- when inside a gh.nvim panel, collpased and expanded node
+					collapse = "zc",
+					-- when cursor is over a "#1234" formatted issue or PR, open its details
+					-- and comments in a new tab.
+					goto_issue = "gd",
+					-- show any details about a node, typically, this reveals commit messages
+					-- and submitted review bodys.
+					details = "d",
+					-- inside a convo buffer, submit a comment
+					submit_comment = "<C-s>",
+					-- inside a convo buffer, when your cursor is ontop of a comment, open
+					-- up a set of actions that can be performed.
+					actions = "<C-a>",
+					-- inside a thread convo buffer, resolve the thread.
+					resolve_thread = "<C-r>",
+					-- inside a gh.nvim panel, if possible, open the node's web URL in your
+					-- browser. useful particularily for digging into external failed CI
+					-- checks.
+					goto_web = "gx",
+				},
+			})
+		end,
+	})
+	use({
+		"f-person/git-blame.nvim",
+		config = function()
+			vim.g.gitblame_enabled = 0
+		end,
+	})
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("gitsigns").setup()
+		end,
+	})
+	use({
+		"ruifm/gitlinker.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("gitlinker").setup()
+		end,
+	})
 end)
