acea683be148b2acc8406b6a227923838456cde7
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-07-05 09:42:23 +0800

    Better github

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -83,50 +83,9 @@ wk.register({
 			end,
 			"Open Git",
 		},
-		h = {
-			name = "+Github",
-			c = {
-				name = "+Commits",
-				c = { "<cmd>GHCloseCommit<cr>", "Close" },
-				e = { "<cmd>GHExpandCommit<cr>", "Expand" },
-				o = { "<cmd>GHOpenToCommit<cr>", "Open To" },
-				p = { "<cmd>GHPopOutCommit<cr>", "Pop Out" },
-				z = { "<cmd>GHCollapseCommit<cr>", "Collapse" },
-			},
-			i = {
-				name = "+Issues",
-				p = { "<cmd>GHPreviewIssue<cr>", "Preview" },
-			},
-			l = {
-				name = "+Litee",
-				t = { "<cmd>LTPanel<cr>", "Toggle Panel" },
-			},
-			r = {
-				name = "+Review",
-				b = { "<cmd>GHStartReview<cr>", "Begin" },
-				c = { "<cmd>GHCloseReview<cr>", "Close" },
-				d = { "<cmd>GHDeleteReview<cr>", "Delete" },
-				e = { "<cmd>GHExpandReview<cr>", "Expand" },
-				s = { "<cmd>GHSubmitReview<cr>", "Submit" },
-				z = { "<cmd>GHCollapseReview<cr>", "Collapse" },
-			},
-			p = {
-				name = "+Pull Request",
-				c = { "<cmd>GHClosePR<cr>", "Close" },
-				d = { "<cmd>GHPRDetails<cr>", "Details" },
-				e = { "<cmd>GHExpandPR<cr>", "Expand" },
-				o = { "<cmd>GHOpenPR<cr>", "Open" },
-				p = { "<cmd>GHPopOutPR<cr>", "PopOut" },
-				r = { "<cmd>GHRefreshPR<cr>", "Refresh" },
-				t = { "<cmd>GHOpenToPR<cr>", "Open To" },
-				z = { "<cmd>GHCollapsePR<cr>", "Collapse" },
-			},
-			t = {
-				name = "+Threads",
-				c = { "<cmd>GHCreateThread<cr>", "Create" },
-				n = { "<cmd>GHNextThread<cr>", "Next" },
-				t = { "<cmd>GHToggleThread<cr>", "Toggle" },
-			},
+		f = {
+			name = "+forge",
+			s = { ":Octo search assignee:SaxJ is:pr<CR>", "Search" },
 		},
 	},
 	t = {
@@ -162,13 +121,14 @@ wk.register({
 	c = {
 		name = "code",
 		a = { ":Lspsaga code_action<CR>", "Action" },
-		r = { "Lspsaga rename<CR>", "Rename" },
-		d = { "Lspsaga show_line_diagnostics<CR>", "Diagnostic" },
+		r = { ":Lspsaga rename<CR>", "Rename" },
+		d = { ":Lspsaga show_line_diagnostics<CR>", "Diagnostic" },
 	},
+	["."] = { ":Neotree current %:p:h:h %:p<CR>", "Files" },
 }, { prefix = "<leader>" })
 
 -- misc
-mapx.nnoremap("<C-e>", ":Neotree<CR>")
+mapx.nnoremap("<C-e>", ":Neotree current %:p:h:h %:p<CR>")
 
 -- Terminal
 mapx.tnoremap("<Esc>", "<C-\\><C-n>")
