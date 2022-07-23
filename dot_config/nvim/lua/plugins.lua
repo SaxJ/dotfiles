0c376df2a5899d78bb1efe6af0d15d4b0252af57
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-07-23 22:05:02 +0800

    Better code folding

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -201,6 +201,25 @@ return require("packer").startup(function(use)
 
 	-- General code plugins
 	use({
+		"kevinhwang91/nvim-ufo",
+		requires = "kevinhwang91/promise-async",
+		config = function()
+			vim.o.foldcolumn = "1"
+			vim.o.foldlevel = 99
+			vim.o.foldlevelstart = -1
+			vim.o.foldenable = true
+
+			vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+			vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
+			require("ufo").setup({
+				provider_selector = function(bufnr, filetype, buftype)
+					return { "treesitter", "indent" }
+				end,
+			})
+		end,
+	})
+	use({
 		"kylechui/nvim-surround",
 		config = function()
 			require("nvim-surround").setup({})
