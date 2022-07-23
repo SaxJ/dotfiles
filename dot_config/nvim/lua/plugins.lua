cd1000ba749806e2b3c190ffcaad0f13b399abcc
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-07-23 19:58:51 +0800

    I dunno

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -36,7 +36,7 @@ return require("packer").startup(function(use)
 		requires = { "kyazdani42/nvim-web-devicons", opt = true },
 	})
 
-	-- Extra Languages
+	-- Languages
 	use("amadeus/vim-mjml")
 	use("rescript-lang/vim-rescript")
 	use({
