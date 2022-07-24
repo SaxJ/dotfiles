e16cc269d4ae9762c891dcd5c6e4edd016053207
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-07-24 18:53:29 +0800

    Improve neogit

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -511,8 +511,6 @@ return require("packer").startup(function(use)
 				disable_hint = false,
 				disable_context_highlighting = false,
 				disable_commit_confirmation = true,
-				-- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
-				-- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
 				auto_refresh = true,
 				disable_builtin_notifications = false,
 				use_magit_keybindings = true,
@@ -537,7 +535,7 @@ return require("packer").startup(function(use)
 						folded = false,
 					},
 					unstaged = {
-						folded = true,
+						folded = false,
 					},
 					staged = {
 						folded = false,
@@ -549,7 +547,7 @@ return require("packer").startup(function(use)
 						folded = true,
 					},
 					unmerged = {
-						folded = false,
+						folded = true,
 					},
 					recent = {
 						folded = true,
@@ -561,8 +559,6 @@ return require("packer").startup(function(use)
 					status = {
 						-- Adds a mapping with "B" as key that does the "BranchPopup" command
 						["B"] = "BranchPopup",
-						-- Removes the default mapping of "s"
-						["s"] = "",
 					},
 				},
 			})
