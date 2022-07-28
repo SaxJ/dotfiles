39b01309aa681f032c8d0769b59a462af84dd495
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-07-28 09:56:31 +0800

    Fold level

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -220,7 +220,7 @@ return require("packer").startup(function(use)
 		config = function()
 			vim.o.foldcolumn = "1"
 			vim.o.foldlevel = 99
-			vim.o.foldlevelstart = -1
+			vim.o.foldlevelstart = 99
 			vim.o.foldenable = true
 
 			vim.keymap.set("n", "zR", require("ufo").openAllFolds)
