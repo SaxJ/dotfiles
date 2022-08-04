61e49650003c593ccffb3bb83d8524ce2bba89b8
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-08-04 21:22:18 +0800

    configure lua language server

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -141,7 +141,22 @@ return require("packer").startup(function(use)
         },
         config = function()
             local lsp = require("lsp-zero")
-            lsp.preset("recommended")
+            lsp.set_preferences({
+                suggest_lsp_servers = true,
+                setup_servers_on_start = true,
+                set_lsp_keymaps = true,
+                configure_diagnostics = true,
+                cmp_capabilities = true,
+                manage_nvim_cmp = true,
+                call_servers = 'mason',
+                sign_icons = {
+                    error = '✘',
+                    warn = '▲',
+                    hint = '⚑',
+                    info = ''
+                }
+            })
+            lsp.configure('sumneko_lua', require('lua-dev').setup())
             lsp.setup()
         end,
     })
