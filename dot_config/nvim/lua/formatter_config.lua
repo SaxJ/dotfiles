038704ef9a5c7989e705bf119a8efe07ff5a1347
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-08-08 16:37:38 +0800

    Format on save

--- dot_config/nvim/lua/formatter_config.lua
+++ dot_config/nvim/lua/formatter_config.lua
@@ -15,6 +15,17 @@ local formatting_options = {
     tsx = {
         require('formatter.filetypes.typescript').prettier
     },
+    ['*'] = {
+        require("formatter.filetypes.any").remove_trailing_whitespace
+    }
 }
+
+vim.cmd([[
+augroup FormatAutogroup
+  autocmd!
+  autocmd User FormatterPre lua print "This will print before formatting"
+  autocmd User FormatterPost lua print "This will print after formatting"
+augroup END
+]])
 
 return formatting_options
