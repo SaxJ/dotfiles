be5a972d6be5a4619993799e6db7935e023228c8
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-08-30 11:38:39 +0800

    Custom haskell indenting

--- dot_doom.d/custom.el
+++ dot_doom.d/custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((ssh-deploy-async . 1)
+   '((haskell-process-use-ghci . t)
+     (haskell-indent-spaces . 4)
+     (ssh-deploy-async . 1)
      (ssh-deploy-async . 0)
      (ssh-deploy-on-explicit-save . 0)
      (ssh-deploy-remote-root . "/ssh:ubuntu@minikube:/home/ubuntu/megatron")))
