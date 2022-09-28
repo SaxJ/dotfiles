b7b194428a77086ed79cb4138b4828edf72d63b2
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-09-28 21:27:57 +0800

    Remove annoying autosession

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -367,15 +367,6 @@ return require("packer").startup(function(use)
             }
         end,
     })
-    use({
-        "rmagatti/auto-session",
-        config = function()
-            require("auto-session").setup({
-                log_level = "error",
-                auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-            })
-        end,
-    })
 
     -- Notes
     use({
