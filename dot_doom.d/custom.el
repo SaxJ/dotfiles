5f9cb2a7975fff80c1acd492e4de2c29604cbf48
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-09-29 21:54:44 +0800

    I guess?

--- dot_doom.d/custom.el
+++ dot_doom.d/custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((ssh-deploy-async . 1)
+   '((haskell-process-use-ghci . t)
+     (haskell-indent-spaces . 4)
+     (ssh-deploy-async . 1)
      (ssh-deploy-async . 0)
      (ssh-deploy-on-explicit-save . 0)
      (ssh-deploy-remote-root . "/ssh:ubuntu@minikube:/home/ubuntu/megatron")))
@@ -14,4 +16,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ts-fold-replacement-face ((t (:foreground nil :box nil :inherit font-lock-comment-face :weight light)))))
