2592e263d2bea8282b0881ed3e16ff789063d690
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-11-01 22:19:09 +0800

    Fix LSP bindings

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -119,6 +119,12 @@ wk.register({
 
 -- misc
 mapx.nnoremap("<C-e>", ":Neotree current %:p:h:h %:p<CR>")
+local bufopts = { noremap = true, silent = true, buffer = bufnr }
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
 
 -- Terminal
 mapx.tnoremap("<Esc>", "<C-\\><C-n>")
