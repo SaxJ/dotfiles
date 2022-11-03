8fee4f2727c560b9158f052dc9051d9b7fe24292
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-11-03 11:12:40 +0800

    Jump to references keybind

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -125,6 +125,7 @@ vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
 vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
 vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 
 -- Terminal
 mapx.tnoremap("<Esc>", "<C-\\><C-n>")
