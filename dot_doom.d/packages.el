032157088cd3e36740dca46e02288fd70cfb7a4a
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-12-18 21:55:25 +0800

    Unpin calendar

--- dot_doom.d/packages.el
+++ dot_doom.d/packages.el
@@ -82,10 +82,16 @@
  tree-sitter
  tree-sitter-langs
 
+ ;; magit
  evil-magit
  forge
  code-review
- magit)
+ magit
+
+ ;; calendar
+ calfw
+ calfw-org
+ org-gcal)
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
