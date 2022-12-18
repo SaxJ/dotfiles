377385b5705e202583277673822a7cc8634a95d3
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-12-18 22:15:41 +0800

    Remove trouble and lspkind, and add gh.nvim

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -29,9 +29,6 @@ return require("packer").startup(function(use)
 	use("arkav/lualine-lsp-progress")
 	use("samjwill/nvim-unception")
 
-	-- Appearance
-	use("kyazdani42/nvim-web-devicons")
-
 	-- Theme
 	use({
 		"folke/tokyonight.nvim",
@@ -119,19 +116,6 @@ return require("packer").startup(function(use)
 
 	-- LSP
 	use({
-		"folke/trouble.nvim",
-		requires = "kyazdani42/nvim-web-devicons",
-		config = function()
-			require("trouble").setup({})
-		end,
-	})
-	use({
-		"onsails/lspkind.nvim",
-		config = function()
-			require("lspkind").init({})
-		end,
-	})
-	use({
 		"hrsh7th/nvim-cmp",
 		requires = {
 			{ "hrsh7th/nvim-cmp" },
@@ -163,7 +147,9 @@ return require("packer").startup(function(use)
 			})
 		end,
 	})
+
 	use("b0o/schemastore.nvim")
+
 	use({
 		"someone-stole-my-name/yaml-companion.nvim",
 		requires = {
@@ -247,6 +233,7 @@ return require("packer").startup(function(use)
 			require("lsp_signature").setup({})
 		end,
 	})
+
 	use({
 		"danymat/neogen",
 		config = function()
@@ -254,6 +241,7 @@ return require("packer").startup(function(use)
 		end,
 		requires = "nvim-treesitter/nvim-treesitter",
 	})
+
 	use({
 		"kevinhwang91/nvim-ufo",
 		requires = "kevinhwang91/promise-async",
@@ -273,19 +261,20 @@ return require("packer").startup(function(use)
 			})
 		end,
 	})
+
 	use({
 		"kylechui/nvim-surround",
 		config = function()
 			require("nvim-surround").setup({})
 		end,
 	})
+
 	use({ "gpanders/editorconfig.nvim" })
+
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 	use({ "nvim-telescope/telescope-file-browser.nvim" })
 	use({ "nvim-telescope/telescope.nvim", requires = { { "nvim-lua/plenary.nvim" } } })
 
-	-- Project Management
-	use({ "airblade/vim-rooter" })
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
@@ -371,15 +360,13 @@ return require("packer").startup(function(use)
 	})
 	use({ "jghauser/mkdir.nvim" })
 
-	-- Code navigation
-
-	-- Version Control
 	use({
 		"f-person/git-blame.nvim",
 		config = function()
 			vim.g.gitblame_enabled = 0
 		end,
 	})
+
 	use({
 		"lewis6991/gitsigns.nvim",
 		requires = {
@@ -396,6 +383,56 @@ return require("packer").startup(function(use)
 			require("neogit").setup({
 				use_magit_keybindings = true,
 				disable_commit_confirmation = true,
+			})
+		end,
+	})
+	use({
+		"ldelossa/gh.nvim",
+		requires = { { "ldelossa/litee.nvim" } },
+		config = function()
+			require("litee.lib").setup()
+			require("litee.gh").setup({
+				-- deprecated, around for compatability for now.
+				jump_mode = "invoking",
+				-- remap the arrow keys to resize any litee.nvim windows.
+				map_resize_keys = false,
+				-- do not map any keys inside any gh.nvim buffers.
+				disable_keymaps = false,
+				-- the icon set to use.
+				icon_set = "default",
+				-- any custom icons to use.
+				icon_set_custom = nil,
+				-- whether to register the @username and #issue_number omnifunc completion
+				-- in buffers which start with .git/
+				git_buffer_completion = true,
+				-- defines keymaps in gh.nvim buffers.
+				keymaps = {
+					-- when inside a gh.nvim panel, this key will open a node if it has
+					-- any futher functionality. for example, hitting <CR> on a commit node
+					-- will open the commit's changed files in a new gh.nvim panel.
+					open = "<CR>",
+					-- when inside a gh.nvim panel, expand a collapsed node
+					expand = "zo",
+					-- when inside a gh.nvim panel, collpased and expanded node
+					collapse = "zc",
+					-- when cursor is over a "#1234" formatted issue or PR, open its details
+					-- and comments in a new tab.
+					goto_issue = "gd",
+					-- show any details about a node, typically, this reveals commit messages
+					-- and submitted review bodys.
+					details = "d",
+					-- inside a convo buffer, submit a comment
+					submit_comment = "<C-s>",
+					-- inside a convo buffer, when your cursor is ontop of a comment, open
+					-- up a set of actions that can be performed.
+					actions = "<C-a>",
+					-- inside a thread convo buffer, resolve the thread.
+					resolve_thread = "<C-r>",
+					-- inside a gh.nvim panel, if possible, open the node's web URL in your
+					-- browser. useful particularily for digging into external failed CI
+					-- checks.
+					goto_web = "gx",
+				},
 			})
 		end,
 	})
