9a782c4a44ab6a6892acbc1db0c7eda455baa49e
Saxon Jensen <saxon.jensen@healthengine.com.au>
2023-01-18 16:58:14 +0800

    Move telescope extensions to root plugins

--- dot_config/nvim/lua/config/plugins/telescope.lua
+++ dot_config/nvim/lua/config/plugins/telescope.lua
@@ -1,81 +1,79 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-        { "nvim-telescope/telescope-file-browser.nvim" },
-        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-        { "someone-stole-my-name/yaml-companion.nvim" },
-        { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("telescope").load_extension("yaml_schema")
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+	},
+	config = function()
+		require("telescope").setup({
+			defaults = {
+				mappings = {
+					i = {
+						["<C-j>"] = "move_selection_next",
+						["<C-k>"] = "move_selection_previous",
+						["<esc>"] = "close",
+					},
+				},
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+				},
+				prompt_prefix = "> ",
+				selection_caret = "> ",
+				entry_prefix = "  ",
+				initial_mode = "insert",
+				selection_strategy = "reset",
+				sorting_strategy = "descending",
+				layout_strategy = "horizontal",
+				layout_config = {
+					horizontal = {
+						mirror = false,
+					},
+					vertical = {
+						mirror = false,
+					},
+				},
+				file_sorter = require("telescope.sorters").get_fuzzy_file,
+				file_ignore_patterns = {},
+				generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+				winblend = 0,
+				border = {},
+				borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+				color_devicons = true,
+				use_less = true,
+				path_display = {},
+				set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+				file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+				grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+				qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+				-- Developer configurations: Not meant for general override
+				buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+				extensions = {
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "smart_case",
+					},
+					file_browser = {
+						hijack_netrw = true,
+					},
+					frecency = {
+						default_workspace = "CWD",
+						ignore_patterns = { "*.git/*", "*/tmp/*", "*/node_modules/*" },
+						show_unindexed = false,
+					},
+				},
+			},
+		})
 
-            require("telescope").setup({
-                defaults = {
-                    mappings = {
-                        i = {
-                            ["<C-j>"] = "move_selection_next",
-                            ["<C-k>"] = "move_selection_previous",
-                            ["<esc>"] = "close",
-                        },
-                    },
-                    vimgrep_arguments = {
-                        "rg",
-                        "--color=never",
-                        "--no-heading",
-                        "--with-filename",
-                        "--line-number",
-                        "--column",
-                        "--smart-case",
-                    },
-                    prompt_prefix = "> ",
-                    selection_caret = "> ",
-                    entry_prefix = "  ",
-                    initial_mode = "insert",
-                    selection_strategy = "reset",
-                    sorting_strategy = "descending",
-                    layout_strategy = "horizontal",
-                    layout_config = {
-                        horizontal = {
-                            mirror = false,
-                        },
-                        vertical = {
-                            mirror = false,
-                        },
-                    },
-                    file_sorter = require("telescope.sorters").get_fuzzy_file,
-                    file_ignore_patterns = {},
-                    generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-                    winblend = 0,
-                    border = {},
-                    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-                    color_devicons = true,
-                    use_less = true,
-                    path_display = {},
-                    set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-                    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-                    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-                    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-                    -- Developer configurations: Not meant for general override
-                    buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
-                    extensions = {
-                        fzf = {
-                            fuzzy = true,
-                            override_generic_sorter = true,
-                            override_file_sorter = true,
-                            case_mode = "smart_case",
-                        },
-                        file_browser = {
-                            hijack_netrw = true,
-                        },
-                        frecency = {
-                            default_workspace = 'CWD',
-                            ignore_patterns = {"*.git/*", "*/tmp/*", "*/node_modules/*"},
-                            show_unindexed = false
-                        }
-                    },
-                },
-            })
-            require("telescope").load_extension("fzf")
-            require("telescope").load_extension("file_browser")
-        end,
-    },
+		require("telescope").load_extension("yaml_schema")
+		require("telescope").load_extension("frecency")
+		require("telescope").load_extension("fzf")
+		require("telescope").load_extension("file_browser")
+	end,
 }
