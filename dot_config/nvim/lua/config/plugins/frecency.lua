4bc00b734ddeb51678569078190b8ac9d60936cc
Saxon Jensen <saxon.jensen@healthengine.com.au>
2023-01-18 10:11:48 +0800

    Install frecency telescope extension

---
+++ dot_config/nvim/lua/config/plugins/frecency.lua
@@ -0,0 +1,9 @@
+return {
+    "nvim-telescope/telescope-frecency.nvim",
+    dependencies = {
+        {"kkharji/sqlite.lua"}
+    },
+    config = function ()
+        require"telescope".load_extension("frecency")
+    end
+}
