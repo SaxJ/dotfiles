d3282a11bdae7ebede5babb5f4d034aaded0a5a3
Saxon Jensen <saxon.jensen@healthengine.com.au>
2023-01-29 22:55:05 +0800

    Fix neorg setup

--- dot_config/nvim/lua/config/plugins/neorg.lua
+++ dot_config/nvim/lua/config/plugins/neorg.lua
@@ -1,24 +1,26 @@
 return {
     "nvim-neorg/neorg",
+    dependencies = { { "nvim-lua/plenary.nvim" } },
     build = ":Neorg sync-parsers",
-    opts = {
-        load = {
-            ["core.defaults"] = {}, -- Loads default behaviour
-            ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
-            ["core.norg.dirman"] = { -- Manages Neorg workspaces
-                config = {
-                    workspaces = {
-                        notes = "~/Documents/wiki/neorg",
+    config = function()
+        require("neorg").setup({
+            load = {
+                ["core.defaults"] = {}, -- Loads default behaviour
+                ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
+                ["core.norg.dirman"] = { -- Manages Neorg workspaces
+                    config = {
+                        workspaces = {
+                            notes = "~/Documents/wiki/neorg",
+                        },
                     },
                 },
+                ["core.presenter"] = {
+                    config = {
+                        zen_mode = "zen-mode",
+                    },
+                },
+                ["core.norg.manoeuvre"] = {},
             },
-            ['core.presenter'] = {
-                config = {
-                    zen_mode = "zen-mode"
-                }
-            },
-            ["core.norg.manoeuvre"] = {},
-        },
-    },
-    dependencies = { { "nvim-lua/plenary.nvim" } },
+        })
+    end,
 }
