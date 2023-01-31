c70b2ab1aaa652c7ede418caf4857e1aea7f59aa
Saxon Jensen <saxon.jensen@healthengine.com.au>
2023-01-31 16:23:23 +0800

    Don't unpin lsp-mode

--- dot_doom.d/packages.el
+++ dot_doom.d/packages.el
@@ -66,8 +66,6 @@
 (package! caddyfile-mode)
 (package! mermaid-mode)
 
-(unpin! lsp-mode)
-
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
