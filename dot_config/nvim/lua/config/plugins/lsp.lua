fcc3bca33fda4778a7f511e00cde9c0380d08336
Saxon Jensen <saxon.jensen@healthengine.com.au>
2023-02-13 21:54:33 +0800

    Setup mason.nvim

--- dot_config/nvim/lua/config/plugins/lsp.lua
+++ dot_config/nvim/lua/config/plugins/lsp.lua
@@ -24,23 +24,67 @@ return {
 		{ "ray-x/lsp_signature.nvim" },
 	},
 	config = function()
-		local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+		local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 		local lsp_attach = function(client, bufnr)
-			local bufopts = { noremap=true, silent=true, buffer=bufnr }
-		  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-		  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-		  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-		  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+			local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
 		end
 
-		local lspconfig = require('lspconfig')
-		require('mason-lspconfig').setup_handlers({
-			function (server_name)
+		local luasnip = require("luasnip")
+		local cmp = require("cmp")
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Up
+				["<C-d>"] = cmp.mapping.scroll_docs(4), -- Down
+				-- C-b (back) C-f (forward) for snippet placeholder navigation.
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true,
+				}),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+			},
+		})
+
+		local lspconfig = require("lspconfig")
+		require('mason').setup()
+		require("mason-lspconfig").setup_handlers({
+			function(server_name)
 				lspconfig[server_name].setup({
 					on_attach = lsp_attach,
-					capabilities = lsp_capabilities
+					capabilities = lsp_capabilities,
 				})
-			end
+			end,
 		})
 	end,
 }
