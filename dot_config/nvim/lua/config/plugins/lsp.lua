7f49602443bed5c8658f58f55d63625addac837a
Saxon Jensen <saxon.jensen@healthengine.com.au>
2023-02-13 21:57:35 +0800

    Fix goto references binding and lua LSP

--- dot_config/nvim/lua/config/plugins/lsp.lua
+++ dot_config/nvim/lua/config/plugins/lsp.lua
@@ -24,11 +24,14 @@ return {
 		{ "ray-x/lsp_signature.nvim" },
 	},
 	config = function()
+	    -- Lua LSP
+	    require('neodev').setup()
+
 		local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 		local lsp_attach = function(client, bufnr)
 			local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+			vim.keymap.set("n", "gD", vim.lsp.buf.references, bufopts)
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
 			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
