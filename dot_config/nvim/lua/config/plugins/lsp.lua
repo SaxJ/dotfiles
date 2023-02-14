0fa046c36c0261bc0a6b51d437877f82db24c8e6
Saxon Jensen <saxon.jensen@healthengine.com.au>
2023-02-14 12:11:41 +0800

    Better lsp

--- dot_config/nvim/lua/config/plugins/lsp.lua
+++ dot_config/nvim/lua/config/plugins/lsp.lua
@@ -1,93 +1,109 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		-- LSP Support
-		{ "williamboman/mason.nvim" },
-		{ "williamboman/mason-lspconfig.nvim" },
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        -- LSP Support
+        { "williamboman/mason.nvim" },
+        { "williamboman/mason-lspconfig.nvim" },
 
-		-- Autocompletion
-		{ "hrsh7th/nvim-cmp" },
-		{ "hrsh7th/cmp-buffer" },
-		{ "hrsh7th/cmp-path" },
-		{ "saadparwaiz1/cmp_luasnip" },
-		{ "hrsh7th/cmp-nvim-lsp" },
-		{ "hrsh7th/cmp-nvim-lua" },
+        -- Autocompletion
+        { "hrsh7th/nvim-cmp" },
+        { "hrsh7th/cmp-buffer" },
+        { "hrsh7th/cmp-path" },
+        { "saadparwaiz1/cmp_luasnip" },
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "hrsh7th/cmp-nvim-lua" },
 
-		-- Snippets
-		{ "L3MON4D3/LuaSnip" },
-		{ "rafamadriz/friendly-snippets" },
+        -- Snippets
+        { "L3MON4D3/LuaSnip" },
+        { "rafamadriz/friendly-snippets" },
 
-		-- Additional tooling
-		{ "MrcJkb/haskell-tools.nvim" },
-		{ "nvim-lua/plenary.nvim" },
-		{ "nvim-telescope/telescope.nvim" },
-		{ "ray-x/lsp_signature.nvim" },
-	},
-	config = function()
-	    -- Lua LSP
-	    require('neodev').setup()
+        -- Additional tooling
+        { "nvim-lua/plenary.nvim" },
+        { "nvim-telescope/telescope.nvim" },
+        { "ray-x/lsp_signature.nvim" },
 
-		local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-		local lsp_attach = function(client, bufnr)
-			local bufopts = { noremap = true, silent = true, buffer = bufnr }
+        -- Enhanced servers
+        { "MrcJkb/haskell-tools.nvim" },
+        { "Hoffs/omnisharp-extended-lsp.nvim" },
+    },
+    config = function()
+        local luasnip = require("luasnip")
+        local cmp = require("cmp")
+        local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+        local lsp_attach = function(client, bufnr)
+            local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-			vim.keymap.set("n", "gD", vim.lsp.buf.references, bufopts)
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-		end
+            vim.keymap.set("n", "gD", vim.lsp.buf.references, bufopts)
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+            vim.keymap.set("n", "gI", vim.lsp.buf.implementation, bufopts)
+        end
 
-		local luasnip = require("luasnip")
-		local cmp = require("cmp")
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Up
-				["<C-d>"] = cmp.mapping.scroll_docs(4), -- Down
-				-- C-b (back) C-f (forward) for snippet placeholder navigation.
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<CR>"] = cmp.mapping.confirm({
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = true,
-				}),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-			}),
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-			},
-		})
+        -- Lua LSP
+        require("neodev").setup()
+        require('haskell-tools').setup({
+            hls = {
+                on_attach = lsp_attach
+            }
+        })
 
-		local lspconfig = require("lspconfig")
-		require('mason').setup()
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				lspconfig[server_name].setup({
-					on_attach = lsp_attach,
-					capabilities = lsp_capabilities,
-				})
-			end,
-		})
-	end,
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-u>"] = cmp.mapping.scroll_docs( -4), -- Up
+                ["<C-d>"] = cmp.mapping.scroll_docs(4), -- Down
+                -- C-b (back) C-f (forward) for snippet placeholder navigation.
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<CR>"] = cmp.mapping.confirm({
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = true,
+                }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable( -1) then
+                        luasnip.jump( -1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+            }),
+            sources = {
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+            },
+        })
+
+        local lspconfig = require("lspconfig")
+        require("mason").setup()
+        require("mason-lspconfig").setup_handlers({
+            function(server_name)
+                lspconfig[server_name].setup({
+                    on_attach = lsp_attach,
+                    capabilities = lsp_capabilities,
+                })
+            end,
+            ["omnisharp"] = function()
+                lspconfig.omnisharp.setup({
+                    handlers = {
+                        ["textDocument/definition"] = require("omnisharp_extended").handler,
+                    },
+                    cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+                })
+            end,
+        })
+    end,
 }
