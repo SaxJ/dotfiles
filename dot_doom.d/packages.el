ed902e2568510828ddd248d3fc2384195a212173
Saxon Jensen <saxon.jensen@healthengine.com.au>
2023-02-22 10:34:47 +0800

    Back to mainline doom

--- dot_doom.d/packages.el
+++ dot_doom.d/packages.el
@@ -67,8 +67,6 @@
 (package! mermaid-mode)
 
 ;; Pin to non-broken packages
-(unpin! magit)
-(unpin! transient)
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
