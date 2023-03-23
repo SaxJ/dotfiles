600be233c13c4b4bbd5da376e5c9f11751b4bb36
Saxon Jensen <saxon.jensen@healthengine.com.au>
2023-03-23 23:20:10 +0800

    Switch to neo-tree for file tree

--- dot_config/nvim/lua/config/plugins/neo-tree.lua
+++ dot_config/nvim/lua/config/plugins/neo-tree.lua
@@ -1,20 +1,10 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v2.x",
-	dependencies = {
-		{ "nvim-lua/plenary.nvim" },
-		{ "kyazdani42/nvim-web-devicons" }, -- not strictly required, but recommended
-		{ "MunifTanjim/nui.nvim" },
-	},
-	config = function()
-		vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-		require("neo-tree").setup({
-			sync_root_with_cwd = true,
-			respect_buf_cwd = true,
-			update_focused_file = {
-				enable = true,
-				update_root = true,
-			},
-		})
-	end,
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {}
+  end,
 }
