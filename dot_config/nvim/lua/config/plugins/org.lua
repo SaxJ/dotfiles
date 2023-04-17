7fdf722fca8b31a92c87bcb4bc6e9d4482989755
Saxon Jensen <saxon.jensen@healthengine.com.au>
2023-04-17 19:38:01 +0800

    Journal capture template

--- dot_config/nvim/lua/config/plugins/org.lua
+++ dot_config/nvim/lua/config/plugins/org.lua
@@ -11,6 +11,13 @@ return {
 		require('orgmode').setup({
 			org_agenda_files = {'~/Documents/wiki/**/*'},
 			org_default_notes_file = '~/Documents/wiki/notes.org',
+			org_capture_templates = {
+				j = {
+					description = "Journal",
+					template = '\n* %<%a, %Y-%m-%d>\n** %<%I:%M %p>\n*** %?',
+					target = os.date('~/Documents/wiki/journals/%Y_01_01.org'),
+				}
+			}
 		})
 		require('org-bullets').setup({})
 		require('headlines').setup({})
