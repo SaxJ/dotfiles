d9e9369e3e6f0521b450b1e88b4f8a896b0734e9
Saxon Jensen <saxon.jensen@healthengine.com.au>
2023-04-27 16:28:11 +0800

    Install project and saga

---
+++ dot_config/nvim/lua/config/plugins/saga.lua
@@ -0,0 +1,11 @@
+return {
+	'nvimdev/lspsaga.nvim',
+	dependencies = {
+      {"nvim-tree/nvim-web-devicons"},
+      {"nvim-treesitter/nvim-treesitter"},
+    },
+    event = "LspAttach",
+	config = function ()
+		require("lspsaga").setup({})
+	end
+}
