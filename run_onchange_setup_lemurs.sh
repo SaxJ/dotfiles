35ff3e3c20d9e36f704243b0bf22806ea8255446
Saxon Jensen <saxon.jensen@healthengine.com.au>
2023-06-06 18:49:02 +0800

    Non-test

--- run_onchange_setup_lemurs.sh
+++ run_onchange_setup_lemurs.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 sudo mkdir -p /etc/lemurs/wms
-printf '#! /bin/sh\nexec spectrwm' | sudo tee /etc/lemurs/wms/spectrwm_test
-sudo chmod +x /etc/lemurs/wms/spectrwm_test
+printf '#! /bin/sh\nexec spectrwm' | sudo tee /etc/lemurs/wms/spectrwm
+sudo chmod +x /etc/lemurs/wms/spectrwm
