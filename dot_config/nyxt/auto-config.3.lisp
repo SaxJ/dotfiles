c187842b4b7bccf484f1ab248778a255680bbc06
Saxon Jensen <saxon.jensen@healthengine.com.au>
2023-06-15 13:19:40 +0800

    Set vi keybinds

--- dot_config/nyxt/auto-config.3.lisp
+++ dot_config/nyxt/auto-config.3.lisp
@@ -1,5 +1,5 @@
+(defmethod customize-instance ((input-buffer input-buffer) &key)
+  (disable-modes* 'nyxt/mode/emacs:emacs-mode input-buffer)
+  (enable-modes* 'nyxt/mode/vi:vi-normal-mode input-buffer))
 (defmethod customize-instance ((browser browser) &key)
   (setf (slot-value browser 'theme) theme:+dark-theme+))
-(defmethod customize-instance ((input-buffer input-buffer) &key)
-  (disable-modes '(nyxt/emacs-mode:emacs-mode) input-buffer)
-  (enable-modes '(nyxt/vi-mode:vi-normal-mode) input-buffer))
