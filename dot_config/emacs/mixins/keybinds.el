480270b7929a1a4e54fad47e4ffa5ca6df87f278
Saxon Jensen <saxon.jensen@healthengine.com.au>
2023-09-07 18:45:58 +0800

    Home grown emacs config

---
+++ dot_config/emacs/mixins/keybinds.el
@@ -0,0 +1,37 @@
+;;; Emacs Bedrock
+;;;
+;;; Mixin: Base UI enhancements
+
+;;; Usage: Append or require this file from init.el to enable various UI/UX
+;;; enhancements.
+
+;;; Contents:
+;;;
+;;;  - Motion aids
+;;;  - Power-ups: Embark and Consult
+;;;  - Minibuffer and completion
+
+(use-package general
+  :ensure t
+  :config
+  (general-evil-setup)
+  (general-nmap
+   :prefix "SPC"
+   "pp" 'project-switch-project
+   "f" 'project-find-file
+   "pf" 'project-find-file
+   "SPC" 'project-find-file
+   
+   "cr" 'eglot-rename
+   "ca" 'eglot-code-actions
+
+   "TAB" 'tabspaces-command-map
+
+   "gg" 'magit
+
+   "oT" 'eat-project)
+
+  (general-nmap
+    "K" 'eglot-hover-eldoc-function
+    "gd" 'eglot-find-typeDefinition
+    "gr" 'eglot-xref-backend))
---
+++ dot_config/emacs/mixins/vim-like.el
@@ -0,0 +1,39 @@
+;;; Emacs Bedrock
+;;;
+;;; Mixin: Vim emulation
+
+;;; Usage: Append or require this file from init.el for bindings in Emacs.
+
+;;; Contents:
+;;;
+;;;  - Core Packages
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;;   Core Packages
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Evil: vi emulation
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-respect-visual-line-mode t)
+  (setq evil-undo-system 'undo-redo)
+
+  (setq evil-want-keybinding nil)       ; prep to load evil-collection
+  (setq evil-want-integration t)
+
+  (setq evil-want-C-u-scroll t)
+  :config
+  (evil-mode)
+
+  ;; Configuring initial major mode for some modes
+  (evil-set-initial-state 'vterm-mode 'emacs))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :custom (evil-collection-setup-minibuffer t)
+  :config
+  (evil-collection-init))
