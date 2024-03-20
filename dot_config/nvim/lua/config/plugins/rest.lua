e1711872153683e841fd7a5a4b6fe6a0feb5bca8
Saxon Jensen <saxon.jensen@healthengine.com.au>
2024-03-20 19:24:30 +0800

    Update neovim toolkit

--- dot_config/nvim/lua/config/plugins/rest.lua
+++ dot_config/nvim/lua/config/plugins/rest.lua
@@ -1,20 +1,8 @@
 return {
-	"NTBBloodbath/rest.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		require("rest-nvim").setup({
-			-- Open request results in a horizontal split
-			result_split_horizontal = false,
-			-- Skip SSL verification, useful for unknown certificates
-			skip_ssl_verification = true,
-			-- Highlight request on run
-			highlight = {
-				enabled = true,
-				timeout = 150,
-			},
-			-- Jump to request line on run
-			jump_to_request = false,
-		})
-		vim.cmd("autocmd FileType http nmap <buffer> <Enter> <Plug>RestNvim")
-	end,
+  "rest-nvim/rest.nvim",
+  ft = "http",
+  dependencies = { "luarocks.nvim" },
+  config = function()
+    require("rest-nvim").setup()
+  end,
 }
--- dot_config/nvim/lua/settings.lua
+++ dot_config/nvim/lua/settings.lua
@@ -57,7 +57,7 @@ vim.diagnostic.config({
 
 -- Autoread files when they're changed
 vim.opt.autoread = true
-vim.api.nvim_create_autocmd({'VimEnter', 'FocusGained', 'BufEnter'}, {
-    group = vim.api.nvim_create_augroup('ReloadFileOnChange', {}),
-    command = 'checktime'
-})
+-- vim.api.nvim_create_autocmd({'VimEnter', 'FocusGained', 'BufEnter'}, {
+--     group = vim.api.nvim_create_augroup('ReloadFileOnChange', {}),
+--     command = 'checktime'
+-- })
