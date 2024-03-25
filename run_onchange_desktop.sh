c50d2d24c5503530b3de7cfc8a782285d51aa231
Saxon Jensen <saxon.jensen@healthengine.com.au>
2024-03-25 22:53:10 +0800

    Change prompt

--- run_onchange_desktop.sh
+++ run_onchange_desktop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read -r -e -p "Install dev packages?" -N 1 install
+read -r -e -p "Install kde updates?" -N 1 install
 
 if [[ "$install" == [Yy]* ]]; then
     paru -S plasma-meta spectacle kmix kdeconnect ktorrent kmail kde-system-meta ark isoimagewriter kgpg konsole
