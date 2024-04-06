0872b28aed43ba5a2e50edfb8931b126ebe297a0
Saxon Jensen <saxon.jensen@healthengine.com.au>
2024-04-06 14:06:33 +0800

    Configure project to make hidden session file

--- dot_config/nvim/lua/config/plugins/project.lua
+++ dot_config/nvim/lua/config/plugins/project.lua
@@ -1,4 +1,9 @@
 return {
     'charludo/projectmgr.nvim',
     lazy = false,
+    config = function()
+        require('projectmgr').setup({
+            session = { enabled = true, file = ".session.vim" }
+        })
+    end
 }
