cb7b507be8c260cc7a7d5d2d8f5ab92ddccb2a15
Saxon Jensen <saxon.jensen@healthengine.com.au>
2024-04-27 14:11:17 +0800

    Removing lots of neovim

--- dot_config/nvim/lua/config/plugins/project.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'charludo/projectmgr.nvim',
-    lazy = false,
-    config = function()
-        require('projectmgr').setup({
-            session = { enabled = true, file = ".idea" }
-        })
-    end
-}
