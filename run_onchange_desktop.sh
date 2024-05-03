decb25ee4e2ceacf780b93cb4d76ff4902d94a6b
Saxon Jensen <saxon.jensen@healthengine.com.au>
2024-05-03 14:06:56 +0800

    Better prompt

--- run_onchange_desktop.sh
+++ run_onchange_desktop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read -r -e -p "Install kde updates?" -N 1 install
+read -r -e -p "Install kde?" -N 1 install
 
 if [[ "$install" == [Yy]* ]]; then
     paru -S --needed plasma-meta spectacle kmix kdeconnect ktorrent kmail kde-system-meta ark isoimagewriter kgpg konsole kwayland-integration plasma5-integration oxygen5 packagekit-qt6 gocryptfs flatpak-kcm geoip2-database kdegraphics-thumbnailers ffmpegthumbs unarchiver unrar
