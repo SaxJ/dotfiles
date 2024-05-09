122d8c819dd849720254a72248f46939ee02570f
Saxon Jensen <saxon.jensen@healthengine.com.au>
2024-05-09 20:00:13 +0800

    Inflection update for neovim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -7,6 +7,24 @@ require("config.lazy")
 local wk = require("which-key")
 local mapx = require("mapx")
 
+local function inflection()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    local word = vim.fn.expand('<cword>')
+    local word_start = vim.fn.matchstrpos(vim.fn.getline('.'), '\\k*\\%' .. (col + 1) .. 'c\\k*')[2]
+
+    if word:find('[a-z][A-Z]') then
+        -- Convert camelCase to snake_case
+        local snake_case_word = word:gsub('([a-z])([A-Z])', '%1_%2'):lower()
+        vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_start + #word, { snake_case_word })
+    elseif word:find('_[a-z]') then
+        -- Convert snake_case to camelCase
+        local camel_case_word = word:gsub('(_)([a-z])', function(_, l) return l:upper() end)
+        vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_start + #word, { camel_case_word })
+    else
+        print("Not a snake_case or camelCase word")
+    end
+end
+
 -- Misc
 wk.register({
     b = {
@@ -16,6 +34,27 @@ wk.register({
         f = {
             vim.lsp.buf.format,
             "Format",
+        },
+    },
+    c = {
+        name = "+code",
+        a = { vim.lsp.buf.code_action, "Code Action" },
+        r = { vim.lsp.buf.rename, "Rename" },
+        d = { require("neogen").generate, "Generate Docs" },
+        i = { inflection, "inflection" },
+    },
+    f = {
+        name = "+files",
+        Y = { ":CopyRelPath<CR>", "Yank Path" },
+    },
+    g = {
+        name = "+git",
+        b = { ":ToggleBlameLine<cr>", "Blame" },
+        g = { ":! zellij run -ci -- lazygit<CR>", "Git" },
+        d = { ":Telescope git_status<CR>", "Changed Files" },
+        h = {
+            name = "+github",
+            p = { ":!gh create pr --web<CR>", "Pull Request" },
         },
     },
     i = {
@@ -29,34 +68,18 @@ wk.register({
         d = { ":Trouble<CR>", "Diagnostics" },
         ["-"] = { ":Oil<CR>", "File Browser" },
     },
-    f = {
-        name = "+files",
-        Y = { ":CopyRelPath<CR>", "Yank Path" },
-    },
-    s = {
-        name = "+search",
-        p = { ":Telescope live_grep hidden=true<cr>", "Search Project" },
-    },
-    g = {
-        name = "+git",
-        b = { ":ToggleBlameLine<cr>", "Blame" },
-        g = { ":! zellij run -ci -- lazygit<CR>", "Git" },
-        d = { ":Telescope git_status<CR>", "Changed Files" },
-        h = {
-            name = "+github",
-            p = { ":!gh create pr --web<CR>", "Pull Request" },
-        },
+    p = {
+        name = "+project",
+        p = { ":Telescope project<CR>", "Projects" },
     },
     r = {
         name = "+remote",
         u = { ":call SyncUploadFile()<cr>", "Upload" },
         d = { ":call SyncDownloadFile()<cr>", "Download" },
     },
-    c = {
-        name = "+code",
-        a = { vim.lsp.buf.code_action, "Code Action" },
-        r = { vim.lsp.buf.rename, "Rename" },
-        d = { require("neogen").generate, "Generate Docs" },
+    s = {
+        name = "+search",
+        p = { ":Telescope live_grep hidden=true<cr>", "Search Project" },
     },
     ["."] = { ":Telescope file_browser path=%:p:h hidden=true<CR>", "Files" },
     ["<leader>"] = { ":Telescope find_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔍<cr>", "Files" },
@@ -71,7 +94,11 @@ mapx.tnoremap("<C-k>", "<Up>")
 mapx.tnoremap("<C-j>", "<Down>")
 
 -- FileType Specific --
-mapx.nmap("<localleader>r", ":HurlRunner<CR>", "silent", { ft = "hurl" })
+-- Hurl
+mapx.nmap("<localleader>e", ":HurlRunner<CR>", "silent", { ft = "hurl" })
+
+-- Json
+mapx.nmap("<localleader>jq", ":Jq<CR>", "silent", { ft = "json" })
 
 mapx.inoremap("C-j", "<C-n>", "silent", { ft = "TelescopePrompt" })
 mapx.inoremap("C-k", "<C-p>", "silent", { ft = "TelescopePrompt" })
