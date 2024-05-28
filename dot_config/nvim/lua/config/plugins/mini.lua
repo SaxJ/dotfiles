65d9e0b5230c6c0448bf2ec848db9d369afe99c2
Saxon Jensen <saxon.jensen@healthengine.com.au>
2024-05-28 23:23:25 +0800

    Add more mini modules

--- dot_config/nvim/lua/config/plugins/mini.lua
+++ dot_config/nvim/lua/config/plugins/mini.lua
@@ -1,12 +1,19 @@
 return {
-    "echasnovski/mini.nvim",
-    config = function ()
-        require('mini.ai').setup({})
-        require('mini.align').setup({})
-        require('mini.bracketed').setup({})
-        require("mini.comment").setup({})
-        require("mini.cursorword").setup({})
-        require("mini.jump").setup({})
-        require("mini.pairs").setup({})
-    end
+	"echasnovski/mini.nvim",
+	config = function()
+		-- General editing
+		require("mini.ai").setup({})
+		require("mini.align").setup({})
+		require("mini.surround").setup({})
+		require("mini.operators").setup({})
+		require("mini.bracketed").setup({})
+		require("mini.comment").setup({})
+
+		-- Tools
+		require("mini.files").setup({})
+		require("mini.cursorword").setup({})
+		require("mini.pairs").setup({})
+		require("mini.git").setup({})
+		require("mini.visits").setup({})
+	end,
 }
