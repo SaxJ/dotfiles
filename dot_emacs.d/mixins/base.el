c209c74d794d43e70cbc62a6c9398d5b7e0154a2
Saxon Jensen <saxon.jensen@healthengine.com.au>
2024-05-28 23:24:59 +0800

    Narrow consult to workspace buffers

--- dot_emacs.d/mixins/base.el
+++ dot_emacs.d/mixins/base.el
@@ -37,32 +37,47 @@
   :bind (("C-x b" . consult-buffer) ; orig. switch-to-buffer
          ("M-y" . consult-yank-pop) ; orig. yank-pop
          ("C-s" . consult-line))
-         :config
-         ;; Narrowing lets you restrict results to certain groups of candidates
-         (setq consult-narrow-key "<"))
+  :config
+  ;; Narrowing lets you restrict results to certain groups of candidates
+  (setq consult-narrow-key "<")
+  (consult-customize consult--source-buffer :hidden t :default nil)
+  (defvar consult--source-workspace
+    (list :name "Workspace Buffers"
+          :narrow ?w
+          :history 'buffer-name-history
+          :category 'buffer
+          :state #'consult--buffer-state
+          :default t
+          :items (lambda () (consult--buffer-query
+                             :predicate #'tabspaces--local-buffer-p
+                             :sort 'visibility
+                             :as #'buffer-name)))
 
-  (use-package embark
-    :ensure t
-    :demand t
-    :after avy
-    :bind (("C-c a" . embark-act))        ; bind this to an easy key to hit
-    :init
-    ;; Add the option to run embark when using avy
-    (defun bedrock/avy-action-embark (pt)
-      (unwind-protect
-          (save-excursion
-            (goto-char pt)
-            (embark-act))
-        (select-window
-         (cdr (ring-ref avy-ring 0))))
-      t)
+    "Set workspace buffer list for consult-buffer")
+  (add-to-list 'consult-buffer-sources 'consult--source-workspace))
 
-    ;; After invoking avy-goto-char-timer, hit "." to run embark at the next
-    ;; candidate you select
-    (setf (alist-get ?. avy-dispatch-alist) 'bedrock/avy-action-embark))
+(use-package embark
+  :ensure t
+  :demand t
+  :after avy
+  :bind (("C-c a" . embark-act))        ; bind this to an easy key to hit
+  :init
+  ;; Add the option to run embark when using avy
+  (defun bedrock/avy-action-embark (pt)
+    (unwind-protect
+        (save-excursion
+          (goto-char pt)
+          (embark-act))
+      (select-window
+       (cdr (ring-ref avy-ring 0))))
+    t)
 
-  (use-package embark-consult
-    :ensure t)
+  ;; After invoking avy-goto-char-timer, hit "." to run embark at the next
+  ;; candidate you select
+  (setf (alist-get ?. avy-dispatch-alist) 'bedrock/avy-action-embark))
+
+(use-package embark-consult
+  :ensure t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -70,91 +85,92 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-  ;; Vertico: better vertical completion for minibuffer commands
-  (use-package vertico
-    :ensure t
-    :init
-    ;; You'll want to make sure that e.g. fido-mode isn't enabled
-    (vertico-mode))
+;; Vertico: better vertical completion for minibuffer commands
+(use-package vertico
+  :ensure t
+  :init
+  ;; You'll want to make sure that e.g. fido-mode isn't enabled
+  (vertico-mode))
 
-  (use-package vertico-directory
-    :after vertico
-    :bind (:map vertico-map
-                ("M-DEL" . vertico-directory-delete-word)))
+(use-package vertico-directory
+  :after vertico
+  :bind (:map vertico-map
+              ("M-DEL" . vertico-directory-delete-word)))
 
-  ;; Marginalia: annotations for minibuffer
-  (use-package marginalia
-    :ensure t
-    :config
-    (marginalia-mode))
+;; Marginalia: annotations for minibuffer
+(use-package marginalia
+  :ensure t
+  :config
+  (marginalia-mode))
 
-  ;; Popup completion-at-point
-  (use-package corfu
-    :ensure t
-    :init
-    (global-corfu-mode)
-    :config
-    (setq corfu-auto t)
-    :bind
-    (:map corfu-map
-          ([tab] . corfu-next)
-          ("TAB" . corfu-next)
-          ([backtab] . corfu-previous)
-          ("S-TAB" . corfu-previous)))
+;; Popup completion-at-point
+(use-package corfu
+  :ensure t
+  :init
+  (global-corfu-mode)
+  :config
+  (setq corfu-auto t)
+  :bind
+  (:map corfu-map
+        ([tab] . corfu-next)
+        ("TAB" . corfu-next)
+        ([backtab] . corfu-previous)
+        ("S-TAB" . corfu-previous)))
 
-  ;; Part of corfu
-  (use-package corfu-popupinfo
-    :after corfu
-    :hook (corfu-mode . corfu-popupinfo-mode)
-    :custom
-    (corfu-popupinfo-delay '(0.25 . 0.1))
-    (corfu-popupinfo-hide nil)
-    :config
-    (corfu-popupinfo-mode))
+;; Part of corfu
+(use-package corfu-popupinfo
+  :after corfu
+  :hook (corfu-mode . corfu-popupinfo-mode)
+  :custom
+  (corfu-popupinfo-delay '(0.25 . 0.1))
+  (corfu-popupinfo-hide nil)
+  :config
+  (corfu-popupinfo-mode))
 
-  ;; Make corfu popup come up in terminal overlay
-  (use-package corfu-terminal
-    :if (not (display-graphic-p))
-    :ensure t
-    :config
-    (corfu-terminal-mode))
+;; Make corfu popup come up in terminal overlay
+(use-package corfu-terminal
+  :if (not (display-graphic-p))
+  :ensure t
+  :config
+  (corfu-terminal-mode))
 
-  ;; Pretty icons for corfu
-  (use-package kind-icon
-    :if (display-graphic-p)
-    :ensure t
-    :after corfu
-    :config
-    (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+;; Pretty icons for corfu
+(use-package kind-icon
+  :if (display-graphic-p)
+  :ensure t
+  :after corfu
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
 
-  (use-package eshell
-    :bind (("C-r" . consult-history)))
+(use-package eshell
+  :bind (("C-r" . consult-history)))
 
-  ;; Orderless: powerful completion style
-  (use-package orderless
-    :ensure t
-    :after prescient
-    :config
-    (setq completion-styles '(orderless prescient)))
+;; Orderless: powerful completion style
+(use-package orderless
+  :ensure t
+  :after prescient
+  :config
+  (setq completion-styles '(orderless prescient)))
 
-  (use-package tabspaces
-    :ensure t
-    :hook (after-init . tabspaces-mode)
-    :config
-    (setq tabspaces-project-switch-commands 'project-find-file
-          tabspaces-initialize-project-with-todo nil))
+(use-package tabspaces
+  :ensure t
+  :hook (after-init . tabspaces-mode)
+  :config
+  (setq tabspaces-project-switch-commands 'project-find-file
+        tabspaces-initialize-project-with-todo nil
+        tabspaces-use-filtered-buffers-as-default t))
 
-  (use-package prescient
-    :ensure t)
+(use-package prescient
+  :ensure t)
 
-  (use-package mood-line
-    :ensure t
-    :config
-    (mood-line-mode)
-    (setq mood-line-glyph-alist mood-line-glyphs-fira-code))
+(use-package mood-line
+  :ensure t
+  :config
+  (mood-line-mode)
+  (setq mood-line-glyph-alist mood-line-glyphs-fira-code))
 
-  (use-package quelpa
-    :ensure t)
-  (use-package quelpa-use-package
-    :ensure t
-    :after quelpa)
+(use-package quelpa
+  :ensure t)
+(use-package quelpa-use-package
+  :ensure t
+  :after quelpa)
