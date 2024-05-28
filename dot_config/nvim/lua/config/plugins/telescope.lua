b42f738a1ac2de2bc3c3bb76834b02f89f8f1145
Saxon Jensen <saxon.jensen@healthengine.com.au>
2024-05-29 00:12:23 +0800

    Remove shit github extension

--- dot_config/nvim/lua/config/plugins/telescope.lua
+++ dot_config/nvim/lua/config/plugins/telescope.lua
@@ -72,6 +72,5 @@ return {
 		require("telescope").load_extension("fzf")
 		require("telescope").load_extension("file_browser")
 		require("telescope").load_extension("project")
-		require("telescope").load_extension("gh")
 	end,
 }
