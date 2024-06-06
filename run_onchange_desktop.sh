d138e7ff55e049139d40ffc50f21868188ce6174
Saxon Jensen <saxon.jensen@healthengine.com.au>
2024-06-06 20:18:27 +0800

    Update KDE

--- run_onchange_desktop.sh
+++ run_onchange_desktop.sh
@@ -2,5 +2,5 @@
 read -r -e -p "Install kde?" -N 1 install
 
 if [[ "$install" == [Yy]* ]]; then
-    paru -S --needed plasma-meta spectacle kmix kdeconnect ktorrent kmail kde-system-meta ark isoimagewriter kgpg konsole kwayland-integration  packagekit-qt6 gocryptfs flatpak-kcm geoip2-database kdegraphics-thumbnailers ffmpegthumbs unarchiver unrar
+    paru -S --needed plasma-meta spectacle kmix kdeconnect ktorrent kmail kde-system-meta ark isoimagewriter kgpg konsole kwayland-integration packagekit-qt6 gocryptfs flatpak-kcm geoip2-database kdegraphics-thumbnailers ffmpegthumbs unarchiver unrar
 fi
