66a444706c4b9f9ba1e80d5576863a01d7c9fcfd
Saxon Jensen <saxon.jensen@healthengine.com.au>
2025-02-04 23:43:34 +0800

    Bye bye mason

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -20,7 +20,6 @@ return {
 	},
 	config = function()
 		local nvim_lsp = require("lspconfig")
-		local mason_lspconfig = require("mason-lspconfig")
 
 		local on_attach = function(client, bufnr)
 			-- format on save
@@ -35,43 +34,36 @@ return {
 			end
 		end
 
-		mason_lspconfig.setup_handlers({
-			function(server)
-				nvim_lsp[server].setup({})
-			end,
-			["lua_ls"] = nvim_lsp["lua_ls"].setup({}),
-			["intelephense"] = nvim_lsp["intelephense"].setup({}),
-			["omnisharp"] = function()
-				nvim_lsp["omnisharp"].setup({
-					cmd = { "omnisharp" },
-					on_attach = on_attach,
-				})
-			end,
-			["ts_ls"] = function()
-				nvim_lsp["ts_ls"].setup({
-					on_attach = on_attach,
-				})
-			end,
-			["cssls"] = function()
-				nvim_lsp["cssls"].setup({
-					on_attach = on_attach,
-				})
-			end,
-			["html"] = function()
-				nvim_lsp["html"].setup({
-					on_attach = on_attach,
-				})
-			end,
-			["jsonls"] = function()
-				nvim_lsp["jsonls"].setup({
-					on_attach = on_attach,
-				})
-			end,
-			["pyright"] = function()
-				nvim_lsp["pyright"].setup({
-					on_attach = on_attach,
-				})
-			end,
+		nvim_lsp["lua_ls"].setup({})
+		nvim_lsp["intelephense"].setup({})
+		nvim_lsp["omnisharp"].setup({
+			cmd = { "omnisharp" },
+			on_attach = on_attach,
 		})
+		nvim_lsp["ts_ls"].setup({
+			on_attach = on_attach,
+		})
+		nvim_lsp["cssls"].setup({
+			on_attach = on_attach,
+		})
+		nvim_lsp["html"].setup({
+			on_attach = on_attach,
+		})
+		nvim_lsp["jsonls"].setup({
+			on_attach = on_attach,
+		})
+		nvim_lsp["pyright"].setup({
+			on_attach = on_attach,
+		})
+    nvim_lsp['ocamllsp'].setup({
+      cmd = {"opam", "exec", "--", "ocamllsp"},
+      on_attach = on_attach,
+    })
+    nvim_lsp['templ'].setup({
+      on_attach = on_attach,
+    })
+    nvim_lsp['go'].setup({
+      on_attach = on_attach,
+    })
 	end,
 }
