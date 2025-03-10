fe94fb52a19ad9f815b22484efcd34f5ca6bcbff
Saxon Jensen <saxon.jensen@healthengine.com.au>
2025-03-10 19:59:33 +0800

    List prs in neogit

--- dot_config/nvim/after/ftplugin/NeogitStatus.lua
+++ dot_config/nvim/after/ftplugin/NeogitStatus.lua
@@ -4,3 +4,9 @@ vim.keymap.set(
 	":! gh pr create --web<CR>",
 	{ noremap = true, silent = true, buffer = true, desc = "Create PR" }
 )
+vim.keymap.set(
+	"n",
+	"@lp",
+	":Octo pr list<CR>",
+	{ noremap = true, silent = true, buffer = true, desc = "Create PR" }
+)
