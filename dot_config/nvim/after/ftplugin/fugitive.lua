5b28efe315268f08997ae0055590a0f72ac3b8bf
SaxJ <saxon.jensen@gmail.com>
2025-05-22 20:39:21 +0800

    Fugitive binds in neovim

---
+++ dot_config/nvim/after/ftplugin/fugitive.lua
@@ -0,0 +1,13 @@
+vim.keymap.set(
+	"n",
+	"q",
+	":bd<CR>",
+	{ noremap = true, silent = true, buffer = true, desc = "Exit" }
+)
+
+vim.keymap.set(
+	"n",
+	"q",
+	":bd<CR>",
+	{ noremap = true, silent = true, buffer = true, desc = "Exit" }
+)
