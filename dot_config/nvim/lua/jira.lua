b58658b204a18d952c376fe59b4c2def99fbf807
SaxJ <saxon.jensen@gmail.com>
2025-05-23 11:44:34 +0800

    Jira issue view details binding

--- dot_config/nvim/lua/jira.lua
+++ dot_config/nvim/lua/jira.lua
@@ -10,6 +10,18 @@ vim.api.nvim_create_user_command("JiraList", function()
   vim.api.nvim_set_option_value('tabstop', 8, {buf = buf})
 
   vim.keymap.set('n', 'q', ':bd<CR>', {buffer = buf})
+  vim.keymap.set('n', 'v', function()
+    local info_buf = vim.api.nvim_create_buf(false, true)
+    local line = vim.api.nvim_get_current_line()
+    local key = vim.fn.split(line)[1]
+
+    vim.api.nvim_win_set_buf(win, info_buf)
+    vim.cmd(string.format(".! jira issue view %s --plain", key))
+
+    vim.keymap.set('n', 'q', ':bd<CR>', {buffer = info_buf})
+    vim.keymap.set('n', 'o', string.format(':! jira open %s<CR>', key))
+    vim.api.nvim_set_option_value('modifiable', false, {buf = info_buf})
+  end, {buffer = buf})
 
   vim.cmd([[.! jira issue list -s "~✅ Done" --columns key,status,summary --plain --no-headers]])
   vim.api.nvim_set_option_value('modifiable', false, {buf = buf})
