c2f1d86e95abe7688a6baaf8ed3b58e555839c74
SaxJ <saxon.jensen@gmail.com>
2025-06-12 21:11:14 +0800

    Better aliases

--- bash/.bashrc.d/alias.sh
+++ bash/.bashrc.d/alias.sh
@@ -1,5 +1,16 @@
 alias vim=nvim
-alias cd=z
 alias lg=lazygit
-alias hx=helix
+
 alias ytd="yt-dlp -t aac -o \"~/Music/%(title)s.%(ext)s\" --restrict-filenames"
+
+alias gs="git status"
+alias gd="git diff"
+alias gc="git commit"
+
+alias vpnc="openvpn3 session-start --config ~/office.ovpn"
+alias vpnd="openvpn3 session-manage --disconnect --config ~/office.ovpn"
+
+function cd() {
+  z "$1"
+  zellij action rename-tab "$(basename "$(pwd)")"
+}
