0b12a7be66ff626f8fa56940293d3b1a8946a8e8
SaxJ <saxon.jensen@gmail.com>
2025-06-17 17:42:54 +0800

    Jira alias

--- bash/.bashrc.d/alias.sh
+++ bash/.bashrc.d/alias.sh
@@ -2,6 +2,7 @@ alias vim=nvim
 alias lg=lazygit
 alias v=nvim
 
+alias ji="jira issue list --jql=\"assignee = currentUser() AND project = MKT AND statusCategory != Done\""
 alias ytd="yt-dlp -t aac -o \"~/Music/%(title)s.%(ext)s\" --restrict-filenames"
 
 alias gs="git status"
