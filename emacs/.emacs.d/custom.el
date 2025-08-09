a714cb79612fb071b8ea615bcb5649cb76a44545
SaxJ <saxon.jensen@gmail.com>
2025-08-09 19:56:27 +0800

    More hurl-ts-mode

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -12,22 +12,7 @@
  '(ignored-local-variable-values '((checkdoc-allow-quoting-nil-and-t . t)))
  '(image-use-external-converter t)
  '(mu4e-search-results-limit -1)
- '(package-selected-packages
-   '(apheleia avy build cape casual chezmoi confluence-markup-mode
-              corfu-terminal csproj-mode csv-mode denote dired-preview
-              dslide dune eat ef-themes eglot-fsharp elm-mode
-              embark-consult evil-collection evil-commentary evil-org
-              evil-surround forge general git-timemachine
-              graphql-ts-mode graphviz-dot-mode haskell-mode helm-org
-              hurl-mode jira jiralib2 jq-mode json-mode
-              kubernetes-evil lastfm marginalia mingus mood-line mpris
-              mu4e-alert multi-vterm nerd-icons-corfu nvm orderless
-              org-modern org-music org-ql org-roam origami otpp plz
-              popper pr-review prescient prodigy rainbow-mode
-              soundcloud string-inflection sudo-edit templ-ts-mode
-              terraform-mode todotxt tuareg vc-use-package vertico
-              vue-mode web-mode yaml-mode yasnippet yeetube yuck-mode
-              zone-nyan zone-rainbow))
+ '(package-selected-packages nil)
  '(package-vc-selected-packages
    '((confluence-markup-mode :url
                              "https://github.com/rmloveland/confluence-markup-mode"
