fa2c36b44aac3041d5b10df2ee86654f58f989ce
SaxJ <saxon.jensen@gmail.com>
2025-10-07 20:54:49 +0800

    Package update

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -13,27 +13,37 @@
  '(image-use-external-converter t)
  '(mu4e-search-results-limit -1)
  '(package-selected-packages
-   '(aidermacs apheleia avy build cape casual chatgpt-shell chezmoi
-               confluence-markup-mode corfu-terminal csproj-mode
-               csv-mode denote-journal dired-preview dslide dune eat
-               ef-themes eglot-fsharp ejira3 elfeed-tube ellama
-               elm-mode embark-consult epresent evil-collection
-               evil-commentary evil-org evil-surround forge general
-               git-timemachine gptel graphql-ts-mode graphviz-dot-mode
-               haskell-mode helm-org hide-mode-line httprepl hurl-mode
-               jira jiralib2 jiralib3 jq-mode js-doc json-mode kubed
-               kubernetes-evil lastfm magit-prime marginalia meow
-               mingus mood-line mpdel mpdmacs mpris mu4easy
-               multi-vterm nerd-icons-corfu nvm ordered-set orderless
-               org-modern org-music org-ql org-roam org-tree-slide
-               origami otpp ox-jira3 pocket-reader pomo-cat popper
-               pr-review prescient prodigy pug-mode rainbow-mode
-               simple-mpc soundcloud string-inflection sudo-edit
-               templ-ts-mode terraform-mode tinee tramp tramp-term
-               tuareg vc-use-package vertico vue-mode web-mode wgrep
-               yaml-mode yasnippet yeetube yuck-mode zone-nyan
-               zone-rainbow))
- '(package-vc-selected-packages '((ejira3 :url "https://git.sr.ht/~madearl/ejira3")))
+   '(apheleia avy build cape casual chezmoi confluence-markup-mode
+              corfu-terminal csproj-mode csv-mode denote dired-preview
+              dslide dune eat ef-themes eglot-fsharp elfeed-tube
+              elm-mode embark-consult evil-collection evil-commentary
+              evil-org evil-surround forge general git-timemachine
+              gptel graphql-ts-mode graphviz-dot-mode haskell-mode
+              helm-org hide-mode-line hurl-mode jira jiralib2 jq-mode
+              json-mode kubed lastfm magit-prime marginalia mood-line
+              mpris mu4e-alert multi-vterm nerd-icons-corfu nvm
+              orderless org-modern org-music org-ql org-roam
+              org-tree-slide origami otpp plz pomo-cat popper
+              pr-review prescient prodigy pug-mode rainbow-mode
+              simple-mpc soundcloud string-inflection sudo-edit
+              templ-ts-mode terraform-mode tinee tuareg vc-use-package
+              vertico vue-mode web-mode wgrep yaml-mode yasnippet
+              yeetube yuck-mode zone-nyan zone-rainbow))
+ '(package-vc-selected-packages
+   '((tinee :url "https://codeberg.org/tusharhero/tinee.git")
+     (mpris :url "https://code.tecosaur.net/tec/mpris.el.git" :branch
+            "main")
+     (confluence-markup-mode :url
+                             "https://github.com/rmloveland/confluence-markup-mode"
+                             :branch "master")
+     (build :url "https://github.com/SaxJ/build.el" :branch "master")
+     (org-music :url "https://github.com/debanjum/org-music" :branch
+                "master")
+     (org-ql :url "https://github.com/alphapapa/org-ql" :branch
+             "master")
+     (hurl-mode :url "https://github.com/JasZhe/hurl-mode")
+     (vc-use-package :vc-backend Git :url
+                     "https://github.com/slotThe/vc-use-package")))
  '(safe-local-variable-values
    '((ssh-deploy-async . 0)
      (ssh-deploy-root-remote
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -201,7 +201,22 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(eglot-confirm-server-edits nil nil nil "Customized with use-package eglot")
- '(package-vc-selected-packages '((ejira3 :url "https://git.sr.ht/~madearl/ejira3"))))
+ '(helm-minibuffer-history-key "M-p")
+ '(package-vc-selected-packages
+   '((tinee :url "https://codeberg.org/tusharhero/tinee.git")
+     (mpris :url "https://code.tecosaur.net/tec/mpris.el.git" :branch
+            "main")
+     (confluence-markup-mode :url
+                             "https://github.com/rmloveland/confluence-markup-mode"
+                             :branch "master")
+     (build :url "https://github.com/SaxJ/build.el" :branch "master")
+     (org-music :url "https://github.com/debanjum/org-music" :branch
+                "master")
+     (org-ql :url "https://github.com/alphapapa/org-ql" :branch
+             "master")
+     (hurl-mode :url "https://github.com/JasZhe/hurl-mode")
+     (vc-use-package :vc-backend Git :url
+                     "https://github.com/slotThe/vc-use-package"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
