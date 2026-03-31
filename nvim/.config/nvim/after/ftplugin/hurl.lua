e3827de09e01623cf6311119d1c04f0309254829
SaxJ <saxon.jensen@gmail.com>
2026-03-31 21:35:44 +0800

    Neovim execute hurl with C-C C-C

--- nvim/.config/nvim/after/ftplugin/hurl.lua
+++ nvim/.config/nvim/after/ftplugin/hurl.lua
@@ -1,6 +1,4 @@
-vim.keymap.set(
-	"n",
-	"<localleader>x",
-	"<cmd>HurlRunner<CR>",
-	{ noremap = true, silent = true, buffer = true, desc = "Execute" }
-)
+vim.keymap.set("n", "<C-c><C-c>", function()
+	local file_name = vim.fn.expand("%:p")
+	vim.cmd([[vsplit | terminal hurl ]] .. file_name)
+end, { noremap = true, silent = true, buffer = true, desc = "Execute" })
