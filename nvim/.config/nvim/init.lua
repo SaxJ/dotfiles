3d3856524028d0b6ad014afc8ff1d1d38d3466b1
SaxJ <saxon.jensen@gmail.com>
2026-04-16 23:17:52 +0800

    Snippets

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -258,30 +258,9 @@ vim.pack.add({
 vim.cmd("colorscheme tokyonight-night")
 
 -- Completion
-local has_words_before = function()
-	local col = vim.api.nvim_win_get_cursor(0)[2]
-	if col == 0 then
-		return false
-	end
-	local line = vim.api.nvim_get_current_line()
-	return line:sub(col, col):match("%s") == nil
-end
-
 require("blink.cmp").setup({
 	keymap = {
-		preset = "none",
-
-		-- If completion hasn't been triggered yet, insert the first suggestion; if it has, cycle to the next suggestion.
-		["<Tab>"] = {
-			function(cmp)
-				if has_words_before() then
-					return cmp.insert_next()
-				end
-			end,
-			"fallback",
-		},
-		-- Navigate to the previous suggestion or cancel completion if currently on the first one.
-		["<S-Tab>"] = { "insert_prev" },
+		preset = "enter",
 	},
 	completion = {
 		menu = { enabled = true },
